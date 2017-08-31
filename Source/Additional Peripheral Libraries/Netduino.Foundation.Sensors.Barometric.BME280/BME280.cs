393c0fb01f8806470294c0caa286d2f46f101ead
Nevyn <mark@thepcsite.co.uk>
2017-08-31 09:17:35 +0100

    Initial versions of the BME280 project.

---
+++ Source/Additional Peripheral Libraries/Netduino.Foundation.Sensors.Barometric.BME280/BME280.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace Netduino.Foundation.Sensors.Barometric.BME280
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
