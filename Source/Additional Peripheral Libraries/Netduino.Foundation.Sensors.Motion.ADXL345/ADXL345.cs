cc940eda48b3c466261c9cc685f82f7d7c354b61
Mark Stevens <mark@thepcsite.co.uk>
2017-09-24 17:19:06 +0100

    Started the ADXL345 class.

--- Source/Additional Peripheral Libraries/Netduino.Foundation.Sensors.Motion.ADXL345/ADXL345.cs
+++ Source/Additional Peripheral Libraries/Netduino.Foundation.Sensors.Motion.ADXL345/ADXL345.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.SPOT;
+using Netduino.Foundation.Core;
 
 namespace Netduino.Foundation.Sensors.Motion
 {
@@ -9,6 +10,8 @@ namespace Netduino.Foundation.Sensors.Motion
     /// </summary>
     public class ADXL345
     {
+		private ICommunictionBus _adxl345;
+
         /// <summary>
         /// Make the default constructor private so that it cannot be used.
         /// </summary>
@@ -21,8 +24,16 @@ namespace Netduino.Foundation.Sensors.Motion
         /// </summary>
         /// <param name="address">Address of the I2C sensor</param>
         /// <param name="speed">Speed of the I2C bus in KHz</param>
-        public ADXL345(byte address, ushort speed)
+        public ADXL345(byte address = 0x1d, ushort speed = 100)
         {
+			if ((address != 0x1d) && (address != 0x53))
+			{
+			}
+			if ((speed != 100) && (speed != 400))
+			{
+			}
+			I2CBus device = new I2CBus(address, speed);
+			_adxl345 = (ICommunicationBus) device;
         }
     }
 }
