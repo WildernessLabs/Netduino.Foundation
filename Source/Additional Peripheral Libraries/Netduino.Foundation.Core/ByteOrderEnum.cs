6c0c2a0545111af22b411a9cc7af12a25aba37ca
Nevyn <mark@thepcsite.co.uk>
2017-09-30 10:13:27 +0100

    Added ByteOrder enum.

---
+++ Source/Additional Peripheral Libraries/Netduino.Foundation.Core/ByteOrderEnum.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Netduino.Foundation.Core
+{
+    /// <summary>
+    /// Indicate the byte ordering for muli-byte words.
+    /// </summary>
+    public enum ByteOrder {  LittleEndian, BigEndian };
+}
