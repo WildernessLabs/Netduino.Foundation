48630c5bb680a0118aaa2787ccae63585f8defa6
Nevyn <mark@thepcsite.co.uk>
2017-10-10 07:11:42 +0100

    Added Servo and 74595 libraries.

---
+++ Source/Netduino.Foundation.IC.74595/ShiftRegister74595.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.SPOT.Hardware;
+using Netduino.Foundation.Core;
+
+namespace Netduino.Foundation.IC
+{
+    /// <summary>
+    /// Provide an interface to connect to a 74595 shift register.
+    /// </summary>
+    /// <remarks>
+    /// Control the outputs from a 74595 shift register (or a chain of shift registers)
+    /// using a SPI interface.
+    /// </remarks>
+    class ShiftRegister74595
+    {
+        #region Member variables / fields
+
+        /// <summary>
+        /// Array containing the bits to be output to the shift register.
+        /// </summary>
+        private bool[] _bits = null;
+
+        /// <summary>
+        /// Number of chips required to implement this ShiftRegister.
+        /// </summary>
+        private int _numberOfChips = 0;
+
+        /// <summary>
+        /// SPI interface used to communicate with the shift registers.
+        /// </summary>
+        private ICommunicationBus _spi = null;
+
+        #endregion Member variables / fields
+
+        #region Constructor(s)
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <remarks>
+        /// This is private to prevent the programmer from calling it explicity.
+        /// </remarks>
+        private ShiftRegister74595()
+        {
+        }
+
+        /// <summary>
+        /// Constructor a ShiftRegister74595 object.
+        /// </summary>
+        /// <param name="bits">Number of bits in the shift register (should be a multiple of 8 bits).</param>
+        /// <param name="spiModule">SPI Module the shift register is attached to.</param>
+        /// <param name="speedKHz">Speed of the bus in kHz (default, 10 KHz).</param>
+        /// <param name="latchPin">GPIO pin to use to latch the data into the shift register.</param>
+        public ShiftRegister74595(int bits, SPI.SPI_module spiModule = SPI.SPI_module.SPI1, uint speedKHz = 10, Cpu.Pin latchPin = Cpu.Pin.GPIO_Pin8)
+        {
+            if ((bits > 0) && ((bits % 8) == 0))
+            {
+                _bits = new bool[bits];
+                _numberOfChips = bits / 8;
+                Clear();
+                var config = new SPI.Configuration(SPI_mod: spiModule, ChipSelect_Port: latchPin,
+                                                   ChipSelect_ActiveState: false, ChipSelect_SetupTime: 0,
+                                                   ChipSelect_HoldTime: 0, Clock_IdleState: true, Clock_Edge: false,
+                                                   Clock_RateKHz: speedKHz);
+
+                SPIBus spi = new SPIBus(config);
+                _spi = (ICommunicationBus)spi;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("ShiftRegister74595: Size must be greater than zero and a multiple of 8 bits");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Overload the index operator to allow the user to get/set a particular 
+        /// bit in the shift register.
+        /// </summary>
+        /// <param name="bit">Bit number to get/set.</param>
+        /// <returns>Value in the specified bit.</returns>
+        public bool this[int bit]
+        {
+            get
+            {
+                if ((bit >= 0) && (bit < _bits.Length))
+                {
+                    return (_bits[bit]);
+                }
+                throw new IndexOutOfRangeException("ShiftRegister74595: Bit index out of range.");
+            }
+            set
+            {
+                if ((bit >= 0) && (bit < _bits.Length))
+                {
+                    _bits[bit] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("ShiftRegister74595: Bit index out of range.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clear all of the bits in the shift register.
+        /// </summary>
+        /// <param name="latch">If true, latch the data after the shift register is cleared (default is false)?</param>
+        public void Clear(bool latch = false)
+        {
+            for (int index = 0; index < _bits.Length; index++)
+            {
+                _bits[index] = false;
+            }
+            if (latch)
+            {
+                LatchData();
+            }
+        }
+
+        /// <summary>
+        /// Send the data to the SPI interface.
+        /// </summary>
+        public void LatchData()
+        {
+            byte[] data = new byte[_numberOfChips];
+
+            for (int chip = 0; chip < _numberOfChips; chip++)
+            {
+                data[chip] = 0;
+                byte bitValue = 1;
+                int offset = chip * 8;
+                for (int bit = 0; bit < 8; bit++)
+                {
+                    if (_bits[offset + bit])
+                    {
+                        data[chip] |= bitValue;
+                    }
+                    bitValue <<= 1;
+                }
+            }
+            _spi.WriteBytes(data);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Netduino.Foundation.Servo.ArduinoLib/Servo.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT;
+
+namespace Netduino.Foundation.Servo.ArduinoLib
+{
+class Servo
+{
+		#region Constants
+
+	/// <summary>
+	/// Default frequency for the servo.
+	/// </summary>
+        public const double SERVO_FREQUENCY = 50;
+
+        /// <summary>
+        /// Default minimum servo angle.
+        /// </summary>
+        public const int MINIMUM_ANGLE = 0;
+
+        /// <summary>
+        /// Default maximum servo angle.
+        /// </summary>
+        public const int MAXIMUM_ANGLE = 180;
+
+        #endregion
+
+        #region Properties
+
+        ///<Summary>
+        ///Determine if this instance of the servo class is associated with a digitsal output pin.
+        ///</Summary>
+        ///<Returns>true if the instance is attached to a PWM pin, false otherwise.</Returns>
+        public bool Attached
+        {
+        	get { return (Pin != Cpu.PWMChannel.PWM_NONE); }
+        }
+
+        /// <summary>
+        /// PWM pin that the servo is attached to.
+        /// </summary>
+        /// <value>PWM pin connected to the servo.</value>
+        private Cpu.PWMChannel Pin { get; set; }
+
+        /// <summary>
+        /// Angle of the servo.
+        /// </summary>
+        /// <value>Angle of the servo in the range 0 to 180 degrees inclusive.</value>
+        private double _angle = 0;
+        public double Angle
+        {
+        	get
+        	{
+        		if (!Attached)
+        		{
+        			throw new InvalidOperationException("Cannot read angle of servo when not attached.");
+        		}
+        		return (_angle);
+        	}
+        	set
+        	{
+        		if ((_angle < MINIMUM_ANGLE) || (_angle > MAXIMUM_ANGLE))
+        		{
+        			throw new ArgumentOutOfRangeException("angle", "Servo angle out of range 0 to 180 degrees.");
+        		}
+        		_angle = value;
+        		double pulseWidth = MinimumPulseWidth + (_angle * ((MaximumPulseWidth - MinimumPulseWidth) / 181));
+        		double dutyCycle = pulseWidth / 20000;
+        		if (PWMPin == null)
+        		{
+        			PWMPin = new PWM(Pin, SERVO_FREQUENCY, dutyCycle, false);
+        			PWMPin.Start();
+        		}
+        		else
+        		{
+        			PWMPin.DutyCycle = dutyCycle;
+        		}
+        	}
+        }
+
+        /// <summary>
+        /// PWM pin used to control the servo.
+        /// </summary>
+        private PWM PWMPin { get; set; }
+
+        /// <summary>
+        /// Minimum pulse width for this servo.  This value normally represents 0 degrees.
+        /// </summary>
+        private int MinimumPulseWidth { get; set; }
+
+        /// <summary>
+        /// Maximum pulse width for the servo.  This value normally represents 180 degrees.
+        /// </summary>
+        /// <value>Pulse width representing 180 degrees.</value>
+        private int MaximumPulseWidth { get; set; }
+
+        #endregion
+
+        #region Contructor(s)
+
+        /// <summary>
+        /// Initialise a new instance of the Servo class.  Use the defaul settings for the properties.
+        /// </summary>
+        /// <remarks>
+        /// Defaults are:
+        ///     Not connected to a pin.
+        ///     Minimum pulse width set to 544
+        ///     Maximum pulse width set to 2400.
+        ///     Minimum angle set to 0 degrees.
+        ///     Maximum angle set to 180 degrees.
+        /// </remarks>
+        public Servo()
+        {
+        	Pin = Cpu.PWMChannel.PWM_NONE;
+        	PWMPin = null;
+        	MinimumPulseWidth = 544;
+        	MaximumPulseWidth = 2400;
+        }
+
+        /// <summary>
+        /// Create a new instance of the Servo class.  This call is equivalent to creating a new instance and
+        /// then calling the <i>Attach</i> method.
+        /// </summary>
+        /// <param name="pin">PWM pin to which the servo is attached.</param>
+        /// <param name="minimum">Minimum value for the pulse width, the default is 544.</param>
+        /// <param name="maximum">Maximum value for the pulse width, the default value is 2400.</param>
+        public Servo(Cpu.PWMChannel pin, int minimum = 544, int maximum = 2400)
+        {
+        	Attach(pin, minimum, maximum);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Attach the servo to a specific PWM pin and set the minimum and maximum pulse
+        /// widths for the 0 and 180 degree angles.
+        /// </summary>
+        /// <param name="pin">PWM pin to use for this servo.</param>
+        /// <param name="minimum">Minimum pulse width for the servo.  The minimum width define the value used for 0 degrees.</param>
+        /// <param name="maximum">Maximum pulse width for the servo.  The maximum value determines the value used for 180 degrees.</param>
+        void Attach(Cpu.PWMChannel pin, int minimum = 544, int maximum = 2400)
+        {
+        	Pin = pin;
+        	MinimumPulseWidth = minimum;
+        	MaximumPulseWidth = maximum;
+        }
+
+        /// <summary>
+        /// Set the angle of the servo to the specified nuber of degrees.
+        /// </summary>
+        /// <param name="angle">Angle for the servo which should be between 0 and 180 degrees inclusive.</param>
+        void Write(int angle)
+        {
+        	Angle = angle;
+        }
+
+        /// <summary>
+        /// Read the angle of the servo.
+        /// </summary>
+        /// <returns>The read.</returns>
+        double Read()
+        {
+        	return (Angle);
+        }
+
+        /// <summary>
+        /// Detach the pin from the servo.
+        /// </summary>
+        void Detach()
+        {
+        	if (Attached)
+        	{
+        		PWMPin.Stop();
+        		PWMPin = null;
+        		Pin = Cpu.PWMChannel.PWM_NONE;
+        	}
+        }
+
+        /// <summary>
+        /// Stop the PWM pulse from this instance of the Servo class.
+        /// </summary>
+        void Stop()
+        {
+        	PWMPin.Stop();
+        }
+
+        /// <summary>
+        /// Set the pulse width to the specified number of microseconds
+        /// </summary>
+        /// <remarks>
+        /// This method is not currently supported.
+        /// </remarks>
+        /// <param name="microseconds">Microsecond pulse width.</param>
+        void WriteMicroseconds(int microseconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
