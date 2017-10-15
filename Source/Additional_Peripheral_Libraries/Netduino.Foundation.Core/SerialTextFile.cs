9b015f77d8cfd3e77fb3214df493d1e35c36189d
Mark Stevens <mark@thepcsite.co.uk>
2017-10-15 18:22:02 +0100

    Generic NMEA and supporting classes started.

---
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Core/SerialTextFile.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+using System.IO.Ports;
+
+namespace Netduino.Foundation.Core
+{
+	/// <summary>
+	/// Provide a mechanism for reading lines of text from a SerialPort.
+	/// </summary>
+	public class SerialTextFile
+	{
+		#region Constants
+
+		/// <summary>
+		/// Default buffer size for the incoming data from the serial port.
+		/// </summary>
+		private const int MAXIMUM_BUFFER_SIZE = 1024;
+
+		#endregion Constants
+
+		#region Member variables / fields
+
+		/// <summary>
+		/// Serial port object that the 
+		/// </summary>
+		SerialPort _serialPort = null;
+
+		/// <summary>
+		/// Buffer to hold the incoming text from the serial port.
+		/// </summary>
+		private string _buffer = String.Empty;
+
+		/// <summary>
+		/// Character(s) that indicate an end of line in the text stream.
+		/// </summary>
+		private string _endOfLine = "\r\n";
+
+		#endregion Member variables / fields
+
+		#region Events and delegates
+
+		/// <summary>
+		/// Delegate for the line ready event.
+		/// </summary>
+		public delegate void LineReady(string line);
+
+		/// <summary>
+		/// A complete line of text has been read, send this to the event subscriber.
+		/// </summary>
+		public event LineReady OnLineReady = null;
+
+		#endregion Events and delegates
+
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor for the SerialTextFile class, made private to prevent the
+		/// programmer from using this method of construcing an object.
+		/// </summary>
+		private SerialTextFile()
+		{
+		}
+
+		/// <summary>
+		/// Create a new SerialTextFile and attach the instance to the specfied serial port.
+		/// </summary>
+		/// <param name="port">Serial port name.</param>
+		/// <param name="baudRate">Baud rate.</param>
+		/// <param name="parity">Parity.</param>
+		/// <param name="dataBits">Data bits.</param>
+		/// <param name="stopBits">Stop bits.</param>
+		/// <param name="endOfLine">Text indicating the end of a line of text.</param>
+		public SerialTextFile(string port, int baudRate, Parity parity, int dataBits, StopBits stopBits, string endOfLine)
+		{
+			_serialPort = new SerialPort(port, baudRate, parity, dataBits, stopBits);
+			_endOfLine = endOfLine;
+			_serialPort.DataReceived += _serialPort_DataReceived;
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		/// <summary>
+		/// Open the serial port and start processing the data from the serial port.
+		/// </summary>
+		public void Open()
+		{
+			if (!_serialPort.IsOpen)
+			{
+				_serialPort.Open();
+			}
+		}
+
+		/// <summary>
+		/// Close the serial port and stop processing data.
+		/// </summary>
+		public void Close()
+		{
+			if (_serialPort.IsOpen)
+			{
+				_serialPort.Close();
+			}
+		}
+
+		#endregion Methods
+
+		#region Interrupt handlers
+
+		/// <summary>
+		/// Process the data from the serial port.
+		/// </summary>
+		void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+            if (e.EventType == SerialData.Chars)
+            {
+                lock (_buffer)
+                {
+                    int amount;
+					byte[] buffer;
+
+					buffer = new byte[MAXIMUM_BUFFER_SIZE];
+                    amount = ((SerialPort) sender).Read(buffer, 0, MAXIMUM_BUFFER_SIZE);
+                    if (amount > 0)
+                    {
+                        if ((amount + _buffer.Length) <= MAXIMUM_BUFFER_SIZE)
+                        {
+							_buffer += Encoding.UTF8.GetChars(buffer);
+                        }
+                        else
+                        {
+                            throw new Exception("Buffer overflow");
+                        }
+                    }
+                }
+            }
+		}
+
+		#endregion Interrupt handlers
+	}
+}
---
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.GPS.NMEA/CGADecoder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Netduino.Foundation.Sensors.GPS;
+
+namespace Netduino.Foundation.Sensors.GenericGPS
+{
+	/// <summary>
+	/// Decoder for CGA messages.
+	/// </summary>
+	public class CGADecoder : INMEADecoder
+	{
+		#region Constructors
+
+		public CGADecoder()
+		{
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		/// <summary>
+		/// Process the data from a CGA message.
+		/// </summary>
+		/// <param name="data">String array of the message components for a CGA message.</param>
+		public void Process(string[] data)
+		{
+		}
+
+		/// <summary>
+		/// Friendly name for the CGA messages.
+		/// </summary>
+		/// <returns>The name.</returns>
+		public string GetName()
+		{
+			return ("Global Postioning System Fixed Data");
+		}
+
+		/// <summary>
+		/// Prefix for the CGA decoder.
+		/// </summary>
+		/// <returns>The prefix.</returns>
+		public string GetPrefix()
+		{
+			return ("$GPCGA");
+		}
+
+		#endregion Methods
+	}
+}
