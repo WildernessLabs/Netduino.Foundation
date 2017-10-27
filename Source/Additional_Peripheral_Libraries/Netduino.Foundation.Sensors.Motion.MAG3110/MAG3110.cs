a8e7989b3876c8b70c2552dddc9519dc7ccab441
Nevyn <mark@thepcsite.co.uk>
2017-10-27 16:16:38 +0100

    MAG3110 - Added interrupt handler functionality.

--- Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Motion.MAG3110/MAG3110.cs
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Motion.MAG3110/MAG3110.cs
@@ -1,4 +1,5 @@
-﻿using Netduino.Foundation.Devices;
+﻿using Microsoft.SPOT.Hardware;
+using Netduino.Foundation.Devices;
 using System;
 
 namespace Netduino.Foundation.Sensors.Motion
@@ -17,12 +18,46 @@ namespace Netduino.Foundation.Sensors.Motion
 
         #endregion Enums
 
+        #region Structures
+
+        /// <summary>
+        /// Sensor readings to be passed back when an interrupt is generated.
+        /// </summary>
+        public struct SensorReading
+        {
+            public short X;
+            public short Y;
+            public short Z;
+        }
+
+        #endregion Structures
+
+        #region Delegates and events
+
+        /// <summary>
+        /// Delegate for the OnDataReceived event.
+        /// </summary>
+        /// <param name="sensorReading">Sensor readings from the MAG3110.</param>
+        public delegate void ReadingComplete(SensorReading sensorReading);
+
+        /// <summary>
+        /// Generated when the sensor indicates that data is ready for processing.
+        /// </summary>
+        public event ReadingComplete OnReadingComplete = null;
+
+        #endregion Delegates and events
+
         #region Member variables / fields
 
         /// <summary>
         /// MAG3110 object.
         /// </summary>
         private I2CBus _mag3110 = null;
+
+        /// <summary>
+        /// Interrupt port used to detect then end of a conversion.
+        /// </summary>
+        private InterruptPort _interruptPort = null;
 
         #endregion Member variables / fields
 
@@ -88,6 +123,52 @@ namespace Netduino.Foundation.Sensors.Motion
             }
         }
 
+        /// <summary>
+        /// Indicate if there is any data ready for reading (x, y or z).
+        /// </summary>
+        /// <remarks>
+        /// See section 5.1.1 of the datasheet.
+        /// </remarks>
+        public bool DataReady
+        {
+            get
+            {
+               return((_mag3110.ReadRegister((byte) Registers.DRStatus) & 0x08) > 0);
+            }
+        }
+
+        /// <summary>
+        /// Enable or disable interrupts.
+        /// </summary>
+        /// <remarks>
+        /// Interrupts can be triggered when a conversion completes (see section 4.2.5 
+        /// of the datasheet).  The interrupts are tied to the ZYXDR bit in the DR Status
+        /// register.
+        /// </remarks>
+        private bool _interruptsEnabled = false;
+        public bool InterruptsEnabled
+        {
+            get
+            {
+                return (_interruptsEnabled);
+            }
+            set
+            {
+                Standby = true;
+                byte cr2 = _mag3110.ReadRegister((byte) Registers.Control2);
+                if (value)
+                {
+                    cr2 |= 0x80;
+                }
+                else
+                {
+                    cr2 &= 0x7f;
+                }
+                _mag3110.WriteRegister((byte) Registers.Control2, cr2);
+                _interruptsEnabled = value;
+            }
+        }
+
         #endregion
 
         #region Constructors
@@ -104,13 +185,20 @@ namespace Netduino.Foundation.Sensors.Motion
         /// </summary>
         /// <param name="address">Address of the MAG3110 (default = 0x0e).</param>
         /// <param name="speed">Speed of the I2C bus (default = 400 KHz).</param>
-        public MAG3110(byte address = 0x0e, ushort speed = 400)
+        /// <param name="interruptPin">Interrupt pin used to detect end of conversions.</param>
+        public MAG3110(byte address = 0x0e, ushort speed = 400, Cpu.Pin interruptPin = Cpu.Pin.GPIO_NONE)
         {
             _mag3110 = new I2CBus(address, speed);
             byte deviceID = _mag3110.ReadRegister((byte) Registers.WhoAmI);
             if (deviceID != 0xc4)
             {
                 throw new Exception("Unknown device ID, " + deviceID.ToString() + " retruend, 0xc4 expected");
+            }
+            if (interruptPin != Cpu.Pin.GPIO_NONE)
+            {
+                _interruptPort = new InterruptPort(interruptPin, false, Microsoft.SPOT.Hardware.Port.ResistorMode.Disabled, 
+                                                   Microsoft.SPOT.Hardware.Port.InterruptMode.InterruptEdgeHigh);
+                _interruptPort.OnInterrupt += _interruptPort_OnInterrupt;
             }
             Reset();
         }
@@ -131,7 +219,7 @@ namespace Netduino.Foundation.Sensors.Motion
         }
 
         /// <summary>
-        /// Force the sensor to make a reading and update the relevant properties.
+        /// Force the sensor to make a reading and update the relevanyt properties.
         /// </summary>
         public void Read()
         {
@@ -145,5 +233,25 @@ namespace Netduino.Foundation.Sensors.Motion
         }
 
         #endregion
+
+        #region Interrupt handlers
+
+        /// <summary>
+        /// Interrupt from the MAG3110 conversion complete interrupt.
+        /// </summary>
+        void _interruptPort_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            if (OnReadingComplete != null)
+            {
+                Read();
+                SensorReading readings = new SensorReading();
+                readings.X = X;
+                readings.Y = Y;
+                readings.Z = Z;
+                OnReadingComplete(readings);
+            }
+        }
+
+        #endregion Interrupt handlers
     }
 }
--- Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Motion.MAG3110/readme.md
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Motion.MAG3110/readme.md
@@ -12,6 +12,7 @@ In it's basic configuration the magnetometer requires four connections:
 | GND          | GND            | Black      |
 | SC           | SCK            | Blue       |
 | SD           | SDA            | White      |
+| D8           | INT1           | Not shown  |
 
 ![MAG3110 on Breadboard](MAG3110OnBreadboard.png)
 
@@ -45,8 +46,51 @@ namespace MAG3110Test
     }
 }
 ```
+This library also supports interrupts when the sensor completes a reading:
+
+```csharp
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Motion;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using System.Threading;
+
+namespace MAG3110Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("MAG3110 Test Application");
+            MAG3110 mag3110 = new MAG3110(0x0e, 400, Pins.GPIO_PIN_D8);
+            mag3110.OnReadingComplete += mag3110_OnReadingComplete;
+            mag3110.InterruptsEnabled = true;
+            mag3110.Standby = false;
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        /// <summary>
+        /// Catch the data ready interrupt and display the data.
+        /// </summary>
+        /// <param name="sensorReading">SensorReadings structure containing the X, Y and X values.</param>
+        static void mag3110_OnReadingComplete(MAG3110.SensorReading sensorReading)
+        {
+            Debug.Print("Reading: x = " + sensorReading.X.ToString() + ", y = " + sensorReading.Y.ToString() + ", z = " + sensorReading.Z.ToString());
+        }
+    }
+}
+```
 
 ## API
+
+This library supports polling and interrupts
+
+### Structures
+
+#### `SensorReading`
+
+This structure is used to pass sensor readings to the interrupt handler.  The structure holds the `X`, `Y` and `Z` sensor readings.
+
+See the `OnReadingComplete` interrupt.
 
 ### Constructors
 
@@ -77,3 +121,9 @@ Put the sensor into standby mode and reset the sensor control and offset registe
 #### `Read()`
 
 Read sensor values from the magnetometer and set the `X`, `Y` and `Z` properties.
+
+### Interrupt Handlers
+
+#### `OnReadingComplete`
+
+Generated when the DR Status register indicates that the sensor has completed a reading and the data is available.  The sensor readings are passed to the interrupt handler through a `SensorReading` structure.
