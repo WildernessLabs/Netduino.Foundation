34d3be941117235fef7fea0016feaf7f6bc679f8
Nevyn <mark@thepcsite.co.uk>
2017-11-08 16:13:59 +0000

    MicroLCD - Added project to peripherals solution

--- Source/Additional_Peripheral_Libraries/Netduino.Foundation.Displays.MicroLiquidCrystal/Properties/AssemblyInfo.cs
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Displays.MicroLiquidCrystal/Properties/AssemblyInfo.cs
@@ -1,29 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MicroLiquidCrystal")]
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Netduino.Foundation.Displays.MicroLiquidCrystal")]
 [assembly: AssemblyDescription("")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Szymon Kobalczyk")]
-[assembly: AssemblyProduct("MicroLiquidCrystal")]
-[assembly: AssemblyCopyright("Copyright © Szymon Kobalczyk 2010")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
