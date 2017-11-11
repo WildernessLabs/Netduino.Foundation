a5942278900f02b29e6952bcc51a46b5caebb49e
Nevyn <mark@thepcsite.co.uk>
2017-11-11 08:32:10 +0000

    ADXL345 - Converted enums to class

--- Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Motion.ADXL345/ADXL345.cs
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Motion.ADXL345/ADXL345.cs
@@ -1,154 +1,175 @@
-﻿using Netduino.Foundation.Devices;
-using System;
+﻿using System;
+using Netduino.Foundation.Devices;
+using Netduino.Foundation.Helpers;
 
 namespace Netduino.Foundation.Sensors.Motion
 {
-    /// <summary>
-    /// Driver for the ADXL345 3-axis digital accelerometer capable of measuring
     //  up to +/-16g.
+    /// <summary>
+    ///     Driver for the ADXL345 3-axis digital accelerometer capable of measuring
     /// </summary>
     public class ADXL345
     {
-        #region Enums
-
-        /// <summary>
-        /// Control registers for the ADXL345 chip.
-        /// </summary>
-        /// <remarks>
-        /// Taken from table 19 on page 23 of the data sheet.
-        /// </remarks>
-        private enum Registers : byte { DeviceID = 0x00, TAPThreshold = 0x1d, OffsetX = 0x1e, OffsetY = 0x1f, OffsetZ = 0x20,
-                                        TAPDuration = 0x21, TAPLatency = 0x22, TAPWindow = 0x23, ActivityThreshold = 0x24, 
-                                        InactivityThreshold = 0x25, InactivityTime = 0x26, ActivityInactivityControl = 0x27,
-                                        FreeFallThreshold = 0x28, FreeFallTime = 0x29, TAPAxes = 0x2a, TAPActivityStatus = 0x2a,
-                                        DataRate = 0x2c, PowerControl = 0x2d, InterruptEnable = 0x2e, InterruptMap = 0x2f,
-                                        InterruptSource = 0x30, DataFormat = 0x31, X0 = 0x32, X1 = 0x33, Y0 = 0x33, Y1 = 0x34,
-                                        Z0 = 0x36, Z1 = 0x37, FirstInFirstOutControl = 0x38, FirstInFirstOutStatus = 0x39};
-
-        /// <summary>
-        /// Possible values for the range (see DataFormat register).
-        /// </summary>
-        /// <remarks>
-        /// See page 27 of the data sheet.
-        /// </remarks>
-        public enum Range { TwoG = 0x00, FourG = 0x01, EightG = 0x02, SixteenG = 0x03 };
-
-        /// <summary>
-        /// Frequency of the sensor readings when the device is in sleep mode.
-        /// </summary>
-        /// <remarks>
-        /// See page 26 of the data sheet.
-        /// </remarks>
-        public enum Frequency { EightHz = 0x00, FourHz = 0x01, TwoHz = 0x02, OneHz = 0x03 };
-
-        #endregion Enums
-
         #region Member variables / fields
 
         /// <summary>
-        /// Communication bus used to communicate with the sensor.
+        ///     Communication bus used to communicate with the sensor.
         /// </summary>
-        private ICommunicationBus _adxl345;
+        private readonly ICommunicationBus _adxl345;
 
         #endregion Member variables / fields
+
+        #region Classes / structures
+
+        /// <summary>
+        ///     Control registers for the ADXL345 chip.
+        /// </summary>
+        /// <remarks>
+        ///     Taken from table 19 on page 23 of the data sheet.
+        /// </remarks>
+        private static class Registers
+        {
+            public static readonly byte ActivityInactivityControl = 0x27;
+            public static readonly byte ActivityThreshold = 0x24;
+            public static readonly byte DataFormat = 0x31;
+            public static readonly byte DataRate = 0x2c;
+            public static readonly byte DeviceID = 0x00;
+            public static readonly byte FirstInFirstOutControl = 0x38;
+            public static readonly byte FirstInFirstOutStatus = 0x39;
+            public static readonly byte FreeFallThreshold = 0x28;
+            public static readonly byte FreeFallTime = 0x29;
+            public static readonly byte InactivityThreshold = 0x25;
+            public static readonly byte InactivityTime = 0x26;
+            public static readonly byte InterruptEnable = 0x2e;
+            public static readonly byte InterruptMap = 0x2f;
+            public static readonly byte InterruptSource = 0x30;
+            public static readonly byte OffsetX = 0x1e;
+            public static readonly byte OffsetY = 0x1f;
+            public static readonly byte OffsetZ = 0x20;
+            public static readonly byte PowerControl = 0x2d;
+            public static readonly byte TAPActivityStatus = 0x2a;
+            public static readonly byte TAPAxes = 0x2a;
+            public static readonly byte TAPDuration = 0x21;
+            public static readonly byte TAPLatency = 0x22;
+            public static readonly byte TAPThreshold = 0x1d;
+            public static readonly byte TAPWindow = 0x23;
+            public static readonly byte X0 = 0x32;
+            public static readonly byte X1 = 0x33;
+            public static readonly byte Y0 = 0x33;
+            public static readonly byte Y1 = 0x34;
+            public static readonly byte Z0 = 0x36;
+            public static readonly byte Z1 = 0x37;
+        }
+
+        /// <summary>
+        ///     Possible values for the range (see DataFormat register).
+        /// </summary>
+        /// <remarks>
+        ///     See page 27 of the data sheet.
+        /// </remarks>
+        public static class Range
+        {
+            public static readonly byte TwoG = 0x00;
+            public static readonly byte FourG = 0x01;
+            public static readonly byte EightG = 0x02;
+            public static readonly byte SixteenG = 0x03;
+        }
+
+        /// <summary>
+        ///     Frequency of the sensor readings when the device is in sleep mode.
+        /// </summary>
+        /// <remarks>
+        ///     See page 26 of the data sheet.
+        /// </remarks>
+        public static class Frequency
+        {
+            public static readonly byte EightHz = 0x00;
+            public static readonly byte FourHz = 0x01;
+            public static readonly byte TwoHz = 0x02;
+            public static readonly byte OneHz = 0x03;
+        }
+
+        #endregion Classes / structures
 
         #region Properties
 
         /// <summary>
-        /// Get the device ID, this should return 0xe5.
+        ///     Get the device ID, this should return 0xe5.
         /// </summary>
         public byte DeviceID
         {
             get
             {
-                byte deviceID = _adxl345.ReadRegister((byte) Registers.DeviceID);
+                var deviceID = _adxl345.ReadRegister(Registers.DeviceID);
                 if (deviceID != 0xe5)
                 {
-                    throw new Exception("Invalid device ID: " + deviceID.ToString() + ", expected 229 (0xe5)");
+                    throw new Exception("Invalid device ID: " + deviceID + ", expected 229 (0xe5)");
                 }
-                return (deviceID);
+                return deviceID;
             }
         }
 
         /// <summary>
-        /// Acceleration along the X-axis.
+        ///     Acceleration along the X-axis.
         /// </summary>
         /// <remarks>
-        /// This property will only contain valid data after a call to Read or after
-        /// an interrupt has been generated.
+        ///     This property will only contain valid data after a call to Read or after
+        ///     an interrupt has been generated.
         /// </remarks>
         public short X { get; private set; }
 
         /// <summary>
-        /// Acceleration along the Y-axis.
+        ///     Acceleration along the Y-axis.
         /// </summary>
         /// <remarks>
-        /// This property will only contain valid data after a call to Read or after
-        /// an interrupt has been generated.
+        ///     This property will only contain valid data after a call to Read or after
+        ///     an interrupt has been generated.
         /// </remarks>
         public short Y { get; private set; }
 
         /// <summary>
-        /// Acceleration along the Z-axis.
+        ///     Acceleration along the Z-axis.
         /// </summary>
         /// <remarks>
-        /// This property will only contain valid data after a call to Read or after
-        /// an interrupt has been generated.
+        ///     This property will only contain valid data after a call to Read or after
+        ///     an interrupt has been generated.
         /// </remarks>
         public short Z { get; private set; }
 
         /// <summary>
-        /// Values stored in this register are automatically added to the X reading.
+        ///     Values stored in this register are automatically added to the X reading.
         /// </summary>
         /// <remarks>
-        /// Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g.
+        ///     Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g.
         /// </remarks>
         public sbyte OffsetX
         {
-            get
-            {
-                return ((sbyte) _adxl345.ReadRegister((byte) Registers.OffsetX));
-            }
-            set
-            {
-                _adxl345.WriteRegister((byte) Registers.OffsetX, (byte) value);
-            }
+            get { return (sbyte) _adxl345.ReadRegister(Registers.OffsetX); }
+            set { _adxl345.WriteRegister(Registers.OffsetX, (byte) value); }
         }
 
         /// <summary>
-        /// Values stored in this register are automatically added to the Y reading.
+        ///     Values stored in this register are automatically added to the Y reading.
         /// </summary>
         /// <remarks>
-        /// Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g.
+        ///     Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g.
         /// </remarks>
         public sbyte OffsetY
         {
-            get
-            {
-                return ((sbyte) _adxl345.ReadRegister((byte) Registers.OffsetY));
-            }
-            set
-            {
-                _adxl345.WriteRegister((byte) Registers.OffsetY, (byte) value);
-            }
+            get { return (sbyte) _adxl345.ReadRegister(Registers.OffsetY); }
+            set { _adxl345.WriteRegister(Registers.OffsetY, (byte) value); }
         }
 
         /// <summary>
-        /// Values stored in this register are automatically added to the Z reading.
+        ///     Values stored in this register are automatically added to the Z reading.
         /// </summary>
         /// <remarks>
-        /// Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g.
+        ///     Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g.
         /// </remarks>
         public sbyte OffsetZ
         {
-            get
-            {
-                return ((sbyte) _adxl345.ReadRegister((byte) Registers.OffsetZ));
-            }
-            set
-            {
-                _adxl345.WriteRegister((byte) Registers.OffsetZ, (byte) value);
-            }
+            get { return (sbyte) _adxl345.ReadRegister(Registers.OffsetZ); }
+            set { _adxl345.WriteRegister(Registers.OffsetZ, (byte) value); }
         }
 
         #endregion Properties
@@ -156,29 +177,30 @@ namespace Netduino.Foundation.Sensors.Motion
         #region Constructors
 
         /// <summary>
-        /// Make the default constructor private so that it cannot be used.
+        ///     Make the default constructor private so that it cannot be used.
         /// </summary>
         private ADXL345()
         {
         }
 
         /// <summary>
-        /// Create a new instance of the ADXL345 communicating over the I2C interface.
+        ///     Create a new instance of the ADXL345 communicating over the I2C interface.
         /// </summary>
         /// <param name="address">Address of the I2C sensor</param>
         /// <param name="speed">Speed of the I2C bus in KHz</param>
         public ADXL345(byte address = 0x53, ushort speed = 100)
         {
-			if ((address != 0x1d) && (address != 0x53))
-			{
+            if ((address != 0x1d) && (address != 0x53))
+            {
                 throw new ArgumentOutOfRangeException("address", "ADXL345 address can only be 0x1d or 0x53.");
-			}
-			if ((speed < 10) || (speed > 400))
-			{
-                throw new ArgumentOutOfRangeException("speed", "ADXL345 speed should be between 10 kHz and 400 kHz inclusive.");
-			}
-			I2CBus device = new I2CBus(address, speed);
-			_adxl345 = (ICommunicationBus) device;
+            }
+            if ((speed < 10) || (speed > 400))
+            {
+                throw new ArgumentOutOfRangeException(
+                    "speed", "ADXL345 speed should be between 10 kHz and 400 kHz inclusive.");
+            }
+            var device = new I2CBus(address, speed);
+            _adxl345 = device;
             if (DeviceID != 0xe5)
             {
                 throw new Exception("Invalid device ID.");
@@ -190,14 +212,14 @@ namespace Netduino.Foundation.Sensors.Motion
         #region Methods
 
         /// <summary>
-        /// Set the PowerControl register (see pages 25 and 26 of the data sheet)
+        ///     Set the PowerControl register (see pages 25 and 26 of the data sheet)
         /// </summary>
         /// <param name="linkActivityAndInactivity">Link the activity and inactivity events.</param>
         /// <param name="autoASleep">Enable / disable auto sleep when the activity and inactivity are linked.</param>
         /// <param name="measuring">Enable or disable measurements (turn on or off).</param>
         /// <param name="sleep">Put the part to sleep (true) or run in normal more (false).</param>
         /// <param name="frequency">Frequency of measurements when the part is in sleep mode.</param>
-        public void SetPowerState(bool linkActivityAndInactivity, bool autoASleep, bool measuring, bool sleep, Frequency frequency)
+        public void SetPowerState(bool linkActivityAndInactivity, bool autoASleep, bool measuring, bool sleep, byte frequency)
         {
             byte data = 0;
             if (linkActivityAndInactivity)
@@ -216,28 +238,29 @@ namespace Netduino.Foundation.Sensors.Motion
             {
                 data |= 0x40;
             }
-            data |= (byte) frequency;
-            _adxl345.WriteRegister((byte) Registers.PowerControl, data);
+            data |= frequency;
+            _adxl345.WriteRegister(Registers.PowerControl, data);
         }
 
         /// <summary>
-        /// Configure the data format (see pages 26 and 27 of the data sheet).
+        ///     Configure the data format (see pages 26 and 27 of the data sheet).
         /// </summary>
         /// <param name="selfTest">Put the device into self test mode when true.</param>
         /// <param name="spiMode">Use 3-wire SPI (true) or 4-wire SPI (false).</param>
-        /// <param name="invertInterrupts">Interrupts are active high (false) or active low (true).</param>
-        /// <param name="fullResolution">Set to full resolution (true) or 10-bit mode using the range determined by the range parameter (false).</param>
+        /// <param name="fullResolution">
+        ///     Set to full resolution (true) or 10-bit mode using the range determined by the range
+        ///     parameter (false).
+        /// </param>
         /// <param name="justification">Left-justified when true, right justified with sign extension when false.</param>
         /// <param name="range">Set the range of the sensor to 2g, 4g, 8g or 16g</param>
         /// <remarks>
-        /// The range of the sensor is determined by the following table:
-        /// 
-        /// 0:  +/- 2g
-        /// 1:  +/- 4g
-        /// 2:  +/- 8g
-        /// 3:  +/ 16g
+        ///     The range of the sensor is determined by the following table:
+        ///         0:  +/- 2g
+        ///         1:  +/- 4g
+        ///         2:  +/- 8g
+        ///         3:  +/ 16g
         /// </remarks>
-        public void SetDataFormat(bool selfTest, bool spiMode, bool fullResolution, bool justification, ADXL345.Range range)
+        public void SetDataFormat(bool selfTest, bool spiMode, bool fullResolution, bool justification, byte range)
         {
             byte data = 0;
             if (selfTest)
@@ -252,55 +275,58 @@ namespace Netduino.Foundation.Sensors.Motion
             {
                 data |= 0x04;
             }
-            if ( justification)
+            if (justification)
             {
                 data |= 0x02;
             }
-            data |= (byte) range;
-            _adxl345.WriteRegister((byte) Registers.DataFormat, data);
+            data |= range;
+            _adxl345.WriteRegister(Registers.DataFormat, data);
         }
 
         /// <summary>
-        /// Set the data rate and low power mode for the sensor.
+        ///     Set the data rate and low power mode for the sensor.
         /// </summary>
         /// <param name="dataRate">Data rate for the sensor.</param>
-        /// <param name="lowPower">Setting this to true will enter low power mode (note measurement will encounter more noise in this mode).</param>
+        /// <param name="lowPower">
+        ///     Setting this to true will enter low power mode (note measurement will encounter more noise in
+        ///     this mode).
+        /// </param>
         public void SetDataRate(byte dataRate, bool lowPower)
         {
             if (dataRate > 0xff)
             {
                 throw new ArgumentOutOfRangeException("dataRate", "Data rate should be in the range 0-15 inclusive");
             }
-            byte data = dataRate;
+            var data = dataRate;
             if (lowPower)
             {
                 data |= 0x10;
             }
-            _adxl345.WriteRegister((byte) Registers.DataRate, data);
+            _adxl345.WriteRegister(Registers.DataRate, data);
         }
 
         /// <summary>
-        /// Read the six sensor bytes and set the values for the X, y and Z acceleration.
+        ///     Read the six sensor bytes and set the values for the X, y and Z acceleration.
         /// </summary>
         /// <remarks>
-        /// All six acceleration registers should be read at the same time to ensure consistency
-        /// of the measurements.
+        ///     All six acceleration registers should be read at the same time to ensure consistency
+        ///     of the measurements.
         /// </remarks>
         public void Read()
         {
-            byte[] data = _adxl345.ReadRegisters((byte) Registers.X0, 6);
+            var data = _adxl345.ReadRegisters(Registers.X0, 6);
             X = (short) (data[0] + (data[1] << 8));
             Y = (short) (data[2] + (data[3] << 8));
             Z = (short) (data[4] + (data[5] << 8));
         }
-        
+
         /// <summary>
-        /// Dump the registers to the debug output stream.
+        ///     Dump the registers to the debug output stream.
         /// </summary>
         public void DisplayRegisters()
         {
-            byte[] registers = _adxl345.ReadRegisters((byte) Registers.TAPThreshold, 29);
-            Helpers.DebugInformation.DisplayRegisters((byte) Registers.TAPThreshold, registers);
+            var registers = _adxl345.ReadRegisters(Registers.TAPThreshold, 29);
+            DebugInformation.DisplayRegisters(Registers.TAPThreshold, registers);
         }
 
         #endregion Methods
