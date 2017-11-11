77978199297ea2485a6ec0c497c38c4583693b38
Nevyn <mark@thepcsite.co.uk>
2017-11-11 13:02:57 +0000

    SerialLCD - Code reformatting.

--- Source/Additional_Peripheral_Libraries/Netduino.Foundation.Displays.SerialLCD/SerialLCD.cs
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Displays.SerialLCD/SerialLCD.cs
@@ -1,52 +1,81 @@
 using System;
 using System.IO.Ports;
+using System.Text;
 using System.Threading;
 
 namespace Netduino.Foundation.Displays
 {
     /// <summary>
-    /// Encapsulate the functionality required to control the Sparkfun serial LCD display.
+    ///     Encapsulate the functionality required to control the Sparkfun serial LCD display.
     /// </summary>
     public class SerialLCD : IDisposable
     {
         #region Enums
 
         /// <summary>
-        /// Describe the cursor style to be displayed.
+        ///     Describe the cursor style to be displayed.
         /// </summary>
-        public enum CursorStyle { UnderlineOn = 0x0e, UnderlineOff = 0x0c, BlinkingBoxOn = 0x0d, BlinkingBoxOff = 0x0c }
+        public enum CursorStyle
+        {
+            UnderlineOn = 0x0e,
+            UnderlineOff = 0x0c,
+            BlinkingBoxOn = 0x0d,
+            BlinkingBoxOff = 0x0c
+        }
 
         /// <summary>
-        /// Display power state.
+        ///     Display power state.
         /// </summary>
-        public enum DisplayPowerState { On = 0x0c, Off = 0x08 }
+        public enum DisplayPowerState
+        {
+            On = 0x0c,
+            Off = 0x08
+        }
 
         /// <summary>
-        /// Describe the number of lines and characters on the display.
+        ///     Describe the number of lines and characters on the display.
         /// </summary>
-        public enum LCDDimensions { Characters20Wide = 0x03, Characters16Wide = 0x04, Lines4 = 0x05, Lines2 = 0x06 }
+        public enum LCDDimensions
+        {
+            Characters20Wide = 0x03,
+            Characters16Wide = 0x04,
+            Lines4 = 0x05,
+            Lines2 = 0x06
+        }
 
         /// <summary>
-        /// Possible baud rates for the display.
+        ///     Possible baud rates for the display.
         /// </summary>
-        public enum LCDBaudRate { Baud2400 = 11, Baud4800, Baud9600, Baud14400, Baud19200, Baud38400 };
+        public enum LCDBaudRate
+        {
+            Baud2400 = 11,
+            Baud4800,
+            Baud9600,
+            Baud14400,
+            Baud19200,
+            Baud38400
+        }
 
         /// <summary>
-        /// Direction to move the curosr or the display.
+        ///     Direction to move the curosr or the display.
         /// </summary>
-        public enum Direction { Left, Right };
+        public enum Direction
+        {
+            Left,
+            Right
+        }
 
         #endregion Enums
 
         #region Constants
 
         /// <summary>
-        /// Byte used to prefix the extended PCD display commands.
+        ///     Byte used to prefix the extended PCD display commands.
         /// </summary>
         private const byte EXTENDED_LCD_COMMAND_CHARACTER = 0xfe;
 
         /// <summary>
-        /// Byte used to prefix the interface commands.
+        ///     Byte used to prefix the interface commands.
         /// </summary>
         private const byte CONFIGURATION_COMMAND_CHARACTER = 0x7c;
 
@@ -55,38 +84,38 @@ namespace Netduino.Foundation.Displays
         #region Member variables / fields
 
         /// <summary>
-        /// Comp port being used to communicate with the display.
+        ///     Comp port being used to communicate with the display.
         /// </summary>
-        private SerialPort _comPort;
+        private readonly SerialPort _comPort;
 
         /// <summary>
-        /// Track if Dispose has been called.
+        ///     Track if Dispose has been called.
         /// </summary>
         private bool _disposed;
 
         /// <summary>
-        /// width of the display in columns (normally 16 or 20).
+        ///     width of the display in columns (normally 16 or 20).
         /// </summary>
-        private byte _width;
+        private readonly byte _width;
 
         /// <summary>
-        /// Height of the display in lines (2 or 4).
+        ///     Height of the display in lines (2 or 4).
         /// </summary>
-        private byte _height;
+        private readonly byte _height;
 
         #endregion Member variable / fields
 
         #region Constructors
 
         /// <summary>
-        /// Make the default constructor private to prevent it being called.
+        ///     Make the default constructor private to prevent it being called.
         /// </summary>
         private SerialLCD()
         {
         }
 
         /// <summary>
-        /// Default constructor.
+        ///     Default constructor.
         /// </summary>
         /// <param name="width">Number of characters on a line.</param>
         /// <param name="height">Number of lines on the display.</param>
@@ -95,10 +124,10 @@ namespace Netduino.Foundation.Displays
         /// <param name="parity">Parity to use (deafult is None).</param>
         /// <param name="dataBits">Number of data bits (default is 8 data bits).</param>
         /// <param name="stopBits">Number of stop bits (default is one stop bit).</param>
-        public SerialLCD(byte width = 16, byte height = 2, string port = "COM1", int baudRate = 9600, 
-                         Parity parity = Parity.None, int dataBits = 8, StopBits stopBits = StopBits.One)
+        public SerialLCD(byte width = 16, byte height = 2, string port = "COM1", int baudRate = 9600,
+            Parity parity = Parity.None, int dataBits = 8, StopBits stopBits = StopBits.One)
         {
-            if ((_comPort != null) && (_comPort.IsOpen))
+            if ((_comPort != null) && _comPort.IsOpen)
             {
                 _comPort.Close();
             }
@@ -133,7 +162,7 @@ namespace Netduino.Foundation.Displays
             }
             _width = width;
             _height = height;
-            Write(new byte[] { CONFIGURATION_COMMAND_CHARACTER, characters, CONFIGURATION_COMMAND_CHARACTER, lines });
+            Write(new[] { CONFIGURATION_COMMAND_CHARACTER, characters, CONFIGURATION_COMMAND_CHARACTER, lines });
             Thread.Sleep(10);
         }
 
@@ -142,7 +171,7 @@ namespace Netduino.Foundation.Displays
         #region Implement IDisposable
 
         /// <summary>
-        /// Implement IDisposable.
+        ///     Implement IDisposable.
         /// </summary>
         public void Dispose()
         {
@@ -154,9 +183,12 @@ namespace Netduino.Foundation.Displays
         }
 
         /// <summary>
-        /// Initiate object disposal.
+        ///     Initiate object disposal.
         /// </summary>
-        /// <param name="disposing">Flag used to determine if the method is being called by the runtime (false) or programmatically (true).</param>
+        /// <param name="disposing">
+        ///     Flag used to determine if the method is being called by the runtime (false) or programmatically
+        ///     (true).
+        /// </param>
         private void Dispose(bool disposing)
         {
             if (!_disposed)
@@ -166,7 +198,7 @@ namespace Netduino.Foundation.Displays
                     _comPort.Close();
                     _comPort.Dispose();
                 }
-                _disposed = true;   // Done - prevent accidental or intentional Dispose calls.
+                _disposed = true; // Done - prevent accidental or intentional Dispose calls.
             }
         }
 
@@ -175,7 +207,7 @@ namespace Netduino.Foundation.Displays
         #region Methods
 
         /// <summary>
-        /// Write the buffer of data to the COM port (i.e. the display).
+        ///     Write the buffer of data to the COM port (i.e. the display).
         /// </summary>
         /// <param name="buffer">Bytes of data to be sent to the display.</param>
         private void Write(byte[] buffer)
@@ -184,15 +216,15 @@ namespace Netduino.Foundation.Displays
         }
 
         /// <summary>
-        /// Toggle the splash screen.
+        ///     Toggle the splash screen.
         /// </summary>
         public void ToggleSplashScreen()
         {
-            Write(new byte[] { CONFIGURATION_COMMAND_CHARACTER, 9});
+            Write(new byte[] { CONFIGURATION_COMMAND_CHARACTER, 9 });
         }
 
         /// <summary>
-        /// Set up the splash screen.
+        ///     Set up the splash screen.
         /// </summary>
         public void SetSplashScreen(string line1, string line2)
         {
@@ -204,15 +236,15 @@ namespace Netduino.Foundation.Displays
         }
 
         /// <summary>
-        /// Change the baud rate of the display using the command interface.
+        ///     Change the baud rate of the display using the command interface.
         /// </summary>
         public void SetBaudRate(LCDBaudRate baudRate)
         {
-            Write(new byte[] { CONFIGURATION_COMMAND_CHARACTER, (byte) baudRate });
+            Write(new[] { CONFIGURATION_COMMAND_CHARACTER, (byte) baudRate });
         }
 
         /// <summary>
-        /// Clear the display.
+        ///     Clear the display.
         /// </summary>
         public void Clear()
         {
@@ -221,7 +253,7 @@ namespace Netduino.Foundation.Displays
         }
 
         /// <summary>
-        /// Set the cursor position on the display to the specified column and line.
+        ///     Set the cursor position on the display to the specified column and line.
         /// </summary>
         /// <param name="column">Column on the display to move the cursor to (0-15 or 0-19).</param>
         /// <param name="line">Line on the display to move the cursor to (0-3).</param>
@@ -239,7 +271,7 @@ namespace Netduino.Foundation.Displays
             //  The following calculations are taken from the data on page
             //  3 of the data sheet.
             //
-            byte absoluteCharacterPosition = column;
+            var absoluteCharacterPosition = column;
             switch (line)
             {
                 case 0:
@@ -251,14 +283,14 @@ namespace Netduino.Foundation.Displays
                     absoluteCharacterPosition += _height;
                     break;
                 case 3:
-                    absoluteCharacterPosition += (_width == 16) ? (byte) 80 : (byte) 84;
+                    absoluteCharacterPosition += _width == 16 ? (byte) 80 : (byte) 84;
                     break;
             }
-            Write(new byte[] { EXTENDED_LCD_COMMAND_CHARACTER, (byte)(0x80 + (absoluteCharacterPosition & 0xff)) });
+            Write(new[] { EXTENDED_LCD_COMMAND_CHARACTER, (byte) (0x80 + (absoluteCharacterPosition & 0xff)) });
         }
 
         /// <summary>
-        /// Move the cursor either right or left on the display.
+        ///     Move the cursor either right or left on the display.
         /// </summary>
         /// <param name="direction">Direction to move the cursor, left or right.</param>
         public void MoveCursor(Direction direction)
@@ -274,7 +306,7 @@ namespace Netduino.Foundation.Displays
         }
 
         /// <summary>
-        /// Scroll the contents of the display one character in the specified direction.
+        ///     Scroll the contents of the display one character in the specified direction.
         /// </summary>
         /// <param name="direction">Direction to scroll the display, left or right.</param>
         public void ScrollDisplay(Direction direction)
@@ -290,30 +322,30 @@ namespace Netduino.Foundation.Displays
         }
 
         /// <summary>
-        /// Set the cursor style to underline or block.  The cursor can also be blinking or solid.
+        ///     Set the cursor style to underline or block.  The cursor can also be blinking or solid.
         /// </summary>
         /// <param name="style">New cursor style (Block/Underline, Blinking/Solid).</param>
         public void SetCursorStyle(CursorStyle style)
         {
-            Write(new byte[] { EXTENDED_LCD_COMMAND_CHARACTER, (byte) style });
+            Write(new[] { EXTENDED_LCD_COMMAND_CHARACTER, (byte) style });
         }
 
         /// <summary>
-        /// Display the text at the current cursor position.
+        ///     Display the text at the current cursor position.
         /// </summary>
         /// <param name="text">Text to display.</param>
         public void DisplayText(string text)
         {
-            Write(System.Text.Encoding.UTF8.GetBytes(text));
+            Write(Encoding.UTF8.GetBytes(text));
         }
 
         /// <summary>
-        /// Turn the display on or off.
+        ///     Turn the display on or off.
         /// </summary>
         /// <param name="state">New power state for the display.</param>
         public void SetDisplayVisualState(DisplayPowerState state)
         {
-            Write(new byte[] { EXTENDED_LCD_COMMAND_CHARACTER, (byte) state });
+            Write(new[] { EXTENDED_LCD_COMMAND_CHARACTER, (byte) state });
         }
 
         #endregion Methods
