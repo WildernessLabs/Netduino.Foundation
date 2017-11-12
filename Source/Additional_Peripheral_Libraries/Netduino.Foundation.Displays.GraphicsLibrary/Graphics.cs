4d321ad69a886e84763b38ebd9382fea005d12d2
Nevyn <mark@thepcsite.co.uk>
2017-11-12 12:37:09 +0000

    SSD1306 - Basic hardware comms for SSD1306 and generic graphics library.

---
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Displays.GraphicsLibrary/Graphics.cs
@@ -0,0 +1,247 @@
+﻿using System;
+
+namespace Netduino.Foundation.Displays
+{
+    /// <summary>
+    ///     Provide high level graphics functions
+    /// </summary>
+    public class GraphicsLibrary
+    {
+        #region Member variables / fields
+
+        /// <summary>
+        /// </summary>
+        private readonly IDisplay _display;
+
+        #endregion Member variables / fields
+
+        #region Constructors
+
+        /// <summary>
+        /// </summary>
+        /// <param name="display"></param>
+        public GraphicsLibrary(IDisplay display)
+        {
+            _display = display;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        ///     Draw a line using Bresenhams line drawing algorithm.
+        /// </summary>
+        /// <remarks>
+        ///     Bresenhams line drawing algoritm:
+        ///     https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+        ///     C# Implementation:
+        ///     https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+        /// </remarks>
+        /// <param name="x0">Abscissa of the starting point of the line.</param>
+        /// <param name="y0">Ordinate of the starting point of the line</param>
+        /// <param name="x1">Abscissa of the end point of the line.</param>
+        /// <param name="y1">Ordinate of the end point of the line</param>
+        /// <param name="colored">Turn the pixel on (true) or off (false).</param>
+        public void DrawLine(int x0, int y0, int x1, int y1, bool colored = true)
+        {
+            var steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                int t;
+                t = x0; // swap x0 and y0
+                x0 = y0;
+                y0 = t;
+                t = x1; // swap x1 and y1
+                x1 = y1;
+                y1 = t;
+            }
+            if (x0 > x1)
+            {
+                int t;
+                t = x0; // swap x0 and x1
+                x0 = x1;
+                x1 = t;
+                t = y0; // swap y0 and y1
+                y0 = y1;
+                y1 = t;
+            }
+            var dx = x1 - x0;
+            var dy = Math.Abs(y1 - y0);
+            var error = dx / 2;
+            var ystep = y0 < y1 ? 1 : -1;
+            var y = y0;
+            for (var x = x0; x <= x1; x++)
+            {
+                _display.DrawPixel(steep ? y : x, steep ? x : y, colored);
+                error = error - dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Draw a horizontal line.
+        /// </summary>
+        /// <param name="x0">Abscissa of the starting point of the line.</param>
+        /// <param name="y0">Ordinate of the starting point of the line.</param>
+        /// <param name="length">Length of the line to draw.</param>
+        /// <param name="colored">Turn the pixel on (true) or off (false).</param>
+        public void DrawHorizontalLine(int x0, int y0, int length, bool colored = true)
+        {
+            for (var x = x0; (x - x0) < length; x++)
+            {
+                _display.DrawPixel(x, y0, colored);
+            }
+        }
+
+        /// <summary>
+        ///     Draw a vertical line.
+        /// </summary>
+        /// <param name="x0">Abscissa of the starting point of the line.</param>
+        /// <param name="y0">Ordinate of the starting point of the line.</param>
+        /// <param name="length">Length of the line to draw.</param>
+        /// <param name="colored">Show the line when (true) or off (false).</param>
+        public void DrawVerticalLine(int x0, int y0, int length, bool colored = true)
+        {
+            for (var y = y0; (y - y0) < length; y++)
+            {
+                _display.DrawPixel(x0, y, colored);
+            }
+        }
+
+        /// <summary>
+        ///     Draw a dircle.
+        /// </summary>
+        /// <remarks>
+        ///     This algorithm draws the circle by splitting the full circle into eight
+        ///     segments.
+        ///     This method uses the Midpoint algorithm:
+        ///     https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+        ///     A C# implementation can be found here:
+        ///     https://rosettacode.org/wiki/Bitmap/Midpoint_circle_algorithm#C.23
+        /// </remarks>
+        /// <param name="centerX">Abscissa of the centre point of the circle.</param>
+        /// <param name="centerY">Ordinate of the centre point of the circle.</param>
+        /// <param name="radius">Radius of the circle.</param>
+        /// <param name="colored">Show the circle when true, </param>
+        /// <param name="filled">Draw a filled circle?</param>
+        public void DrawCircle(int centerX, int centerY, int radius, bool colored = true, bool filled = false)
+        {
+            var d = (5 - (radius * 4)) / 4;
+            var x = 0;
+            var y = radius;
+            while (x <= y)
+            {
+                if (filled)
+                {
+                    DrawLine(centerX + x, centerY + y, centerX - x, centerY + y, colored);
+                    DrawLine(centerX + x, centerY - y, centerX - x, centerY - y, colored);
+                    DrawLine(centerX - y, centerY + x, centerX + y, centerY + x, colored);
+                    DrawLine(centerX - y, centerY - x, centerX + y, centerY - x, colored);
+                }
+                else
+                {
+                    _display.DrawPixel(centerX + x, centerY + y, colored);
+                    _display.DrawPixel(centerX + y, centerY + x, colored);
+                    _display.DrawPixel(centerX - y, centerY + x, colored);
+                    _display.DrawPixel(centerX - x, centerY + y, colored);
+                    _display.DrawPixel(centerX - x, centerY - y, colored);
+                    _display.DrawPixel(centerX - y, centerY - x, colored);
+                    _display.DrawPixel(centerX + x, centerY - y, colored);
+                    _display.DrawPixel(centerX + y, centerY - x, colored);
+                }
+                if (d < 0)
+                {
+                    d += (2 * x) + 1;
+                }
+                else
+                {
+                    d += (2 * (x - y)) + 1;
+                    y--;
+                }
+                x++;
+            }
+        }
+
+        /// <summary>
+        ///     Draw a filled dircle.
+        /// </summary>
+        /// <param name="centerX">Abscissa of the centre point of the circle.</param>
+        /// <param name="centerY">Ordinate of the centre point of the circle.</param>
+        /// <param name="radius">Radius of the circle.</param>
+        /// <param name="colored">Show the circle when true, </param>
+        public void DrawFilledCircle(int centerX, int centerY, int radius, bool colored)
+        {
+            DrawCircle(centerX, centerY, radius, colored, true);
+        }
+
+        /// <summary>
+        ///     Draw a rectangle.
+        /// </summary>
+        /// <param name="xLeft">Abscissa of the top left corner.</param>
+        /// <param name="yTop">Ordinate of the top left corner.</param>
+        /// <param name="width">Width of the rectangle.</param>
+        /// <param name="height">Height of the rectangle.</param>
+        /// <param name="colored">Draw the pixel (true) or turn the pixel off (false).</param>
+        /// <param name="filled">Fill the rectangle (true) or draw the outline (false, default).</param>
+        public void DrawRectangle(int xLeft, int yTop, int width, int height, bool colored = true, bool filled = false)
+        {
+            width--;
+            height--;
+            if (filled)
+            {
+                for (var i = 0; i <= height; i++)
+                {
+                    DrawLine(xLeft, yTop + i, xLeft + width, yTop + i, colored);
+                }
+            }
+            else
+            {
+                DrawLine(xLeft, yTop, xLeft + width, yTop, colored);
+                DrawLine(xLeft + width, yTop, xLeft + width, yTop + height, colored);
+                DrawLine(xLeft + width, yTop + height, xLeft, yTop + height, colored);
+                DrawLine(xLeft, yTop, xLeft, yTop + height, colored);
+            }
+        }
+
+        /// <summary>
+        ///     Draw a filled rectangle.
+        /// </summary>
+        /// <param name="xLeft">Abscissa of the top left corner.</param>
+        /// <param name="yTop">Ordinate of the top left corner.</param>
+        /// <param name="width">Width of the rectangle.</param>
+        /// <param name="height">Height of the rectangle.</param>
+        /// <param name="colored">Draw the pixel (true) or turn the pixel off (false).</param>
+        public void DrawFilledRectangle(int xLeft, int yTop, int width, int height, bool colored = true)
+        {
+            DrawRectangle(xLeft, yTop, width, height, colored, true);
+        }
+
+        #endregion Methods
+
+        #region IDisplay
+
+        /// <summary>
+        ///     Show the changes on the display.
+        /// </summary>
+        public void Show()
+        {
+            _display.Show();
+        }
+
+        /// <summary>
+        ///     Clear the display.
+        /// </summary>
+        /// <param name="updateDisplay">Update the display immediately when true.</param>
+        public void Clear(bool updateDisplay = false)
+        {
+            _display.Clear(updateDisplay);
+        }
+
+        #endregion IDisplay
+    }
+}
---
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Displays.SSD1306/SSD1306.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Netduino.Foundation.Devices;
+
+namespace Netduino.Foundation.Displays
+{
+    /// <summary>
+    /// </summary>
+    public class SSD1306 : IDisplay
+    {
+        #region Member variables / fields
+
+        /// <summary>
+        ///     SSD1306 display.
+        /// </summary>
+        private readonly ICommunicationBus _ssd1306;
+
+        /// <summary>
+        ///     Width of the display in pixels.
+        /// </summary>
+        private readonly int _width;
+
+        /// <summary>
+        ///     Height of the display in pixels.
+        /// </summary>
+        private readonly int _height;
+
+        /// <summary>
+        ///     Number of pages in the display.
+        /// </summary>
+        private readonly int _pages;
+
+        /// <summary>
+        ///     Buffer holding the pixels in the display.
+        /// </summary>
+        private readonly byte[] _buffer;
+
+        /// <summary>
+        ///     Sequence of cammnd bytes that must be set to the display before
+        ///     the Show method can send the data buffer.
+        /// </summary>
+        private readonly byte[] _showPreamble;
+
+        #endregion Member variables / fields
+
+        #region Constructors
+
+        /// <summary>
+        ///     Default constructor is private to prevent it being used.
+        /// </summary>
+        private SSD1306()
+        {
+        }
+
+        /// <summary>
+        ///     Create a new SSD1306 object using the default parameters for
+        /// </summary>
+        /// <param name="address">Address of the bus on the I2C display.</param>
+        /// <param name="speed">Speed of the I2C bus.</param>
+        /// <param name="width">Width of the display (default to 128 pixels).</param>
+        /// <param name="height">Height of the display (default to 64 pixels).</param>
+        public SSD1306(byte address = 60, ushort speed = 400, ushort width = 128, ushort height = 64)
+        {
+            var display = new I2CBus(address, speed);
+            _ssd1306 = display;
+            _width = width;
+            _height = height;
+            _pages = _height / 8;
+            _buffer = new byte[width * _pages];
+            //
+            //  Now setup the display.
+            //
+            SendCommands(new byte[]
+            {
+                0xae, 0xd5, 0x80, 0xa8, 0x3f, 0xd3, 0x00, 0x40 | 0x0, 0x8d, 0x14, 0x20, 0x00, 0xa0 | 0x1, 0xc8, 0xda,
+                0x12, 0x81, 0xcf, 0xd9, 0xf1, 0xdb, 0x40, 0xa4, 0xa6, 0xaf
+            });
+            _showPreamble = new byte[] { 0x21, 0x00, (byte) (_width - 1), 0x22, 0x00, (byte) (_pages - 1) };
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        ///     Send a command to the display.
+        /// </summary>
+        /// <param name="command">Command byte to send to the display.</param>
+        private void SendCommand(byte command)
+        {
+            _ssd1306.WriteBytes(new byte[] { 0x00, command });
+        }
+
+        /// <summary>
+        ///     Send a sequence of commands to the display one by one.
+        /// </summary>
+        /// <param name="commands">List of commands to send.</param>
+        private void SendCommands(byte[] commands)
+        {
+            var command = new byte[2];
+            command[0] = 0x00;
+            for (var index = 0; index < commands.Length; index++)
+            {
+                command[1] = commands[index];
+                _ssd1306.WriteBytes(command);
+            }
+        }
+
+        /// <summary>
+        ///     Make the display ready for use.
+        /// </summary>
+        /// <remarks>
+        ///     Currently only 128 x 64 bit SSD1306 OLED displays are supported.
+        /// </remarks>
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        ///     Send buffer to display
+        /// </summary>
+        public void Show()
+        {
+            SendCommands(_showPreamble);
+            //
+            //  Send the buffer page by page.
+            //
+            var data = new byte[17];
+            data[0] = 0x40;
+            for (ushort outer = 0; outer < _buffer.Length; outer += 16)
+            {
+                for (ushort inner = 0; inner < 16; inner++)
+                {
+                    data[inner + 1] = _buffer[outer + inner];
+                }
+                SendCommand(0x40);
+                _ssd1306.WriteBytes(data);
+            }
+        }
+
+        /// <summary>
+        ///     Clear the display buffer.
+        /// </summary>
+        /// <param name="updateDisplay">Immediately update the display when true.</param>
+        public void Clear(bool updateDisplay = false)
+        {
+            for (var index = 0; index < _buffer.Length; index++)
+            {
+                _buffer[index] = 0;
+            }
+            if (updateDisplay)
+            {
+                Show();
+            }
+        }
+
+        /// <summary>
+        ///     Coordinates start with index 0
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="colored">True = turn on pixel, false = turn off pixel</param>
+        public void DrawPixel(int x, int y, bool colored)
+        {
+            DrawPixel((byte) x, (byte) y, colored);
+        }
+
+        /// <summary>
+        ///     Coordinates start with index 0
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="colored">True = turn on pixel, false = turn off pixel</param>
+        public void DrawPixel(byte x, byte y, bool colored)
+        {
+            if ((x >= _width) || (y >= _height))
+            {
+                throw new Exception("DisplayPixel: co-ordinates oout of bounds");
+            }
+            var index = ((y / 8) * _width) + x;
+            if (colored)
+            {
+                _buffer[index] = (byte) (_buffer[index] | (byte) (1 << (y % 8)));
+            }
+            else
+            {
+                _buffer[index] = (byte) (_buffer[index] & ~(byte) (1 << (y % 8)));
+            }
+        }
+
+        #endregion Methods
+    }
+}
