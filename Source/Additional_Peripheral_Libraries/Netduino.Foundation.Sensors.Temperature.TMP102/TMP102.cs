cabc9ae68de56cbd73ca4c1f0c2bb58166b73f23
Nevyn <mark@thepcsite.co.uk>
2017-12-03 17:24:45 +0000

    TMP102 Added code and documentation

--- Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Temperature.TMP102/TMP102.cs
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Temperature.TMP102/TMP102.cs
@@ -1,9 +1,110 @@
-﻿using System;
-using Microsoft.SPOT;
+﻿using Netduino.Foundation.Devices;
 
 namespace Netduino.Foundation.Sensors.Temperature
 {
     public class TMP102
     {
+        #region Enums
+
+        /// <summary>
+        ///     Indicate the resolution of the sensor.
+        /// </summary>
+        public enum Resolution : byte
+        {
+            /// <summary>
+            ///     Operate in 12-bit mode.
+            /// </summary>
+            Resolution12Bits,
+
+            /// <summary>
+            ///     Operate in 13-bit mode.
+            /// </summary>
+            Resolution13Bits
+        }
+
+        #endregion Enums
+
+        #region Member variables / fields
+
+        /// <summary>
+        ///     TMP102 sensor.
+        /// </summary>
+        private readonly ICommunicationBus _tmp102;
+
+        #endregion Member variables / fields
+
+        #region Properties
+
+        /// <summary>
+        ///     Get / set the resolution of the sensor.
+        /// </summary>
+        private Resolution _sensorResolution;
+
+        public Resolution SensorResolution
+        {
+            get { return _sensorResolution; }
+            set
+            {
+                var configuration = _tmp102.ReadRegisters(0x01, 2);
+                if (value == Resolution.Resolution12Bits)
+                {
+                    configuration[1] &= 0xef;
+                }
+                else
+                {
+                    configuration[1] |= 0x10;
+                }
+                _tmp102.WriteRegisters(0x01, configuration);
+                _sensorResolution = value;
+            }
+        }
+
+        /// <summary>
+        ///     Temperature (in degrees centigrade).
+        /// </summary>
+        public double Temperature
+        {
+            get
+            {
+                var temperatureData = _tmp102.ReadRegisters(0x00, 2);
+                var sensorReading = 0;
+                if (SensorResolution == Resolution.Resolution12Bits)
+                {
+                    sensorReading = (temperatureData[0] << 4) | (temperatureData[1] >> 4);
+                }
+                else
+                {
+                    sensorReading = (temperatureData[0] << 5) | (temperatureData[1] >> 3);
+                }
+                return sensorReading * 0.0625;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        ///     Default constructor (private to prevent it being called).
+        /// </summary>
+        private TMP102()
+        {
+        }
+
+        /// <summary>
+        ///     Create a new TMP102 object using the default configuration for the sensor.
+        /// </summary>
+        /// <param name="address">I2C address of the sensor.</param>
+        /// <param name="speed">Speed of the communication with the sensor.</param>
+        public TMP102(byte address = 0x48, ushort speed = 100)
+        {
+            _tmp102 = new I2CBus(address, speed);
+            var configuration = _tmp102.ReadRegisters(0x01, 2);
+            _sensorResolution = (configuration[1] & 0x10) > 0
+                ? Resolution.Resolution13Bits
+                : Resolution.Resolution12Bits;
+        }
+
+        #endregion Constructors
     }
 }
---
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.Sensors.Temperature.TMP102/readme.md
@@ -0,0 +1,62 @@
+# TMP102 - Temperature Sensor
+
+The TMP102 os a temperature sensor capable of reading the current temperature with an accuracy of 0.5C over the range of -25C to 85C with a total range of -40C to 125C.
+
+## Purchasing
+
+TMP102 sensors are available on a breakout board from the following suppliers:
+
+* [Sparkfun TMP102 Breakout Board](https://www.sparkfun.com/products/13314)
+
+## Hardware
+
+TMP102 sensors can be connected to the Netduino using only four connections:
+
+![TMP102 On Breadboard](TMP102OnBreadboard.png)
+
+## Software
+
+The following application reads the temperature from the TMP102 sensor every second and displays the results in the debug console:
+
+```csharp
+using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Temperature;
+
+namespace TMP102Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("TMP102 Test");
+            var tmp102 = new TMP102();
+            while (true)
+            {
+                Debug.Print("Temperature: " + tmp102.Temperature.ToString("f2"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+```
+
+## API
+
+### Enums
+
+#### `enum Resolution`
+
+Allows the sensor to be configured in 12-bit `Resolution12Bits` or 13-bit `Resolution13Bits` mode.
+
+### Constructors
+
+#### `TMP102(byte address = 0x48, ushort speed = 100)`
+
+Create a new `TMP102` object using the default `address` and `speed` settings for the sensor.
+
+### Properties
+
+#### `double Temperature`
+
+Returns the current temperature in degrees centigrade.
