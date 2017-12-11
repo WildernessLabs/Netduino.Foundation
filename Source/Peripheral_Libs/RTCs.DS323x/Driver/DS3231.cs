2387ebd3983b9fe463f331ab352428346b23c9ca
bryan costanich <b@wowzer.net>
2017-12-10 18:29:39 -0800

    Major project and folder reorg and namespace fixups

---
+++ Source/Peripheral_Libs/RTCs.DS323x/Driver/DS3231.cs
@@ -0,0 +1,38 @@
+using Microsoft.SPOT.Hardware;
+using Netduino.Foundation.Devices;
+
+namespace Netduino.Foundation.RTCs
+{
+    /// <summary>
+    ///     Create a new DS3231 Real Time Clock object.
+    /// </summary>
+    public class DS3231 : DS323x
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Default constructor is private to prevent the developer from calling it.
+        /// </summary>
+        private DS3231()
+        {
+        }
+
+        /// <summary>
+        ///     Create a new MAG3110 object using the default parameters for the component.
+        /// </summary>
+        /// <param name="address">Address of the DS3231 (default = 0x68).</param>
+        /// <param name="speed">Speed of the I2C bus (default = 100 KHz).</param>
+        /// <param name="interruptPin">Digital pin connected to the alarm interrupt pin on the RTC.</param>
+        public DS3231(byte address = 0x68, ushort speed = 100, Cpu.Pin interruptPin = Cpu.Pin.GPIO_NONE)
+        {
+            var device = new I2CBus(address, speed);
+            _ds323x = device;
+            if (interruptPin != Cpu.Pin.GPIO_NONE)
+            {
+                InterruptPin = interruptPin;
+            }
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ Source/Peripheral_Libs/Sensors.Atmospheric.SHT31D/Driver/SHT31D.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Netduino.Foundation.Devices;
+
+namespace Netduino.Foundation.Sensors.Atmospheric
+{
+    /// <summary>
+    ///     Provide a mechanism for reading the temperature and humidity from
+    ///     a SHT31D temperature / humidity sensor.
+    /// </summary>
+    /// <remarks>
+    ///     Readings from the sensor are made in Single-shot mode.
+    /// </remarks>
+    public class SHT31D
+    {
+        #region Member variables / fields
+
+        /// <summary>
+        ///     SH31D sensor communicates using I2C.
+        /// </summary>
+        private readonly I2CBus _sht31d;
+
+        #endregion Member variables / fields
+
+        #region Properties
+
+        /// <summary>
+        ///     Get the last humidity reading from the sensor.
+        /// </summary>
+        /// <remarks>
+        ///     The Read method should be called before the data in this property
+        ///     contains valid data.
+        /// </remarks>
+        public float Humidity { get; private set; }
+
+        /// <summary>
+        ///     Get the last temperature reading.
+        /// </summary>
+        /// <remarks>
+        ///     The Read method should be called before the data in this property
+        ///     contains valid data.
+        /// </remarks>
+        public float Temperature { get; private set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        ///     Default constructor (made private to prevent it being called).
+        /// </summary>
+        private SHT31D()
+        {
+        }
+
+        /// <summary>
+        ///     Create a new SHT31D object.
+        /// </summary>
+        /// <param name="address">Sensor address (should be 0x44 or 0x45).</param>
+        /// <param name="speed">Bus speed (0-1000 KHz).</param>
+        public SHT31D(byte address = 0x44, ushort speed = 100)
+        {
+            if ((address != 0x44) && (address != 0x45))
+            {
+                throw new ArgumentOutOfRangeException("address", "Address should be 0x44 or 0x45");
+            }
+            if (speed > 1000)
+            {
+                throw new ArgumentOutOfRangeException("speed", "Speed should be between 0 and 1000 KHz");
+            }
+            _sht31d = new I2CBus(address, speed);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        ///     Get a reading from the sensor and set the Temperature and Humidity properties.
+        /// </summary>
+        public void Read()
+        {
+            var data = _sht31d.WriteRead(new byte[] { 0x2c, 0x06 }, 6);
+            Humidity = (100 * (float) ((data[3] << 8) + data[4])) / 65535;
+            Temperature = ((175 * (float) ((data[0] << 8) + data[1])) / 65535) - 45;
+        }
+
+        #endregion
+    }
+}
