9450b46a25e90f8f818a151bfe292dacc3707bfd
bryan costanich <b@wowzer.net>
2017-12-11 16:48:14 -0800

    added two samples, and a roslyn hack.

---
+++ Source/Peripheral_Libs/Sensors.Atmospheric.BME280/Samples/BME280_Sample/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Atmospheric;
+using System.Threading;
+
+namespace BME280_Sample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BME280 sensor = new BME280();
+
+            string message;
+            while (true)
+            {
+                sensor.Read();
+                message = "Temperature: " + sensor.Temperature.ToString("F1") + " C\n";
+                message += "Humidity: " + sensor.Humidity.ToString("F1") + " %\n";
+                message += "Pressure: " + (sensor.Pressure / 100).ToString("F0") + " hPa\n\n";
+                Debug.Print(message);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+
---
+++ Source/Peripheral_Libs/Sensors.Atmospheric.SHT31D/Samples/SHT31D_Sample/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Atmospheric;
+using System.Threading;
+
+namespace SHT31D_Sample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SHT31D sht31d = new SHT31D();
+
+            Debug.Print("SHT31D Temperature / Humidity Test");
+            while (true)
+            {
+                sht31d.Read();
+                Debug.Print("Temperature: " + sht31d.Temperature.ToString("f2") + ", Humidity: " + sht31d.Humidity.ToString("f2"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
