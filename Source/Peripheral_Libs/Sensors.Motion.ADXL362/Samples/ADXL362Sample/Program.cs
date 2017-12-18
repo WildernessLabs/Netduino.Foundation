1fa244e59fa76bf07c73d1a4b0d9546cea168c1f
Nevyn <mark@thepcsite.co.uk>
2017-12-18 11:16:12 +0000

    ADXL362 - Added sample

---
+++ Source/Peripheral_Libs/Sensors.Motion.ADXL362/Samples/ADXL362Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Motion;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace ADXL362Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("ADXL362 - Test");
+            var sensor = new ADXL362(SPI_Devices.SPI1, Pins.GPIO_PIN_D7, 100);
+            while (true)
+            {
+                sensor.Read();
+                Debug.Print("X: " + sensor.X + ", Y: " + sensor.Y + ", Z: " + sensor.Z);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
