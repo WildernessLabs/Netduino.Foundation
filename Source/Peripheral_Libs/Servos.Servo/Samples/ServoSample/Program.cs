be890904b07a4e132758c37af1400ef9ad1b317c
Nevyn <mark@thepcsite.co.uk>
2017-12-18 11:37:51 +0000

    Servo - Added sample

---
+++ Source/Peripheral_Libs/Servos.Servo/Samples/ServoSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using Netduino.Foundation.Servos;
+
+namespace ServoSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Servo servo = new Servo(PWMChannels.PWM_PIN_D9, 1000, 2000);
+            while (true)
+            {
+                for (int angle = 0; angle <= 180; angle++)
+                {
+                    servo.Angle = angle;
+                    Thread.Sleep(40);
+                }
+                for (int angle = 179; angle > 0; angle--)
+                {
+                    servo.Angle = angle;
+                    Thread.Sleep(40);
+                }
+            }
+        }
+    }
+}
