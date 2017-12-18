c467af905336e9c7a8be8670babdbf4933c1f3ab
Nevyn <mark@thepcsite.co.uk>
2017-12-18 11:33:46 +0000

    Analog Temperature - Added sample

---
+++ Source/Peripheral_Libs/Sensors.Temperature.Analog/Samples/AnalogTemperatureSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Temperature.Analog;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace AnalogTemperatureSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("** Read TMP36 **");
+            var tmp36 = new AnalogTemperatureSensor(AnalogChannels.ANALOG_PIN_A0,
+                                                    AnalogTemperatureSensor.SensorType.TMP36);
+            while (true)
+            {
+                Debug.Print("Reading: " + tmp36.Temperature.ToString("f2"));
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
