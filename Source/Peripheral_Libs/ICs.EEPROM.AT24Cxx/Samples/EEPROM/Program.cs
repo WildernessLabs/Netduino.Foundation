6e364828be239f89e211863d1418c1b2ac1d40c7
Nevyn <mark@thepcsite.co.uk>
2017-12-18 09:33:45 +0000

    AT24Cxx - Added example.

---
+++ Source/Peripheral_Libs/ICs.EEPROM.AT24Cxx/Samples/EEPROM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.ICs.EEPROM;
+
+namespace AT24C32Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var eeprom = new AT24Cxx(0x57);
+            eeprom.Write(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            var memory = eeprom.Read(0, 16);
+            for (ushort index = 0; index < 16; index++)
+            {
+                Debug.Print("Byte: " + index + ", Value: " + memory[index]);
+            }
+            eeprom.Write(3, new byte[] { 10 });
+            eeprom.Write(7, new byte[] { 1, 2, 3, 4 });
+            memory = eeprom.Read(0, 16);
+            for (ushort index = 0; index < 16; index++)
+            {
+                Debug.Print("Byte: " + index + ", Value: " + memory[index]);
+            }
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
