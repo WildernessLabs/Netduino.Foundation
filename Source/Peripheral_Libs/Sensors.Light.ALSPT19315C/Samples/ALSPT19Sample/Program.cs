a5398f476d6279d75b61a97f32564ea4cc416d87
Nevyn <mark@thepcsite.co.uk>
2017-12-18 10:55:23 +0000

    ALSPT19 - Added sample.

---
+++ Source/Peripheral_Libs/Sensors.Light.ALSPT19315C/Samples/ALSPT19Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Netduino.Fountation.Sensors.Light;
+
+namespace ALSPT19315CTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("ALSPT19315C Test");
+            var sensor = new ALSPT19315C(Cpu.AnalogChannel.ANALOG_1, 3.3);
+            while (true)
+            {
+                Debug.Print("Sensor reading: " + sensor.Voltage.ToString("f2"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
