8497d9c66a0eec52d8dac406f760fc4568e7fa82
Nevyn <mark@thepcsite.co.uk>
2017-12-18 10:27:57 +0000

    SHT31D - Updated sample.

---
+++ Source/Peripheral_Libs/Sensors.Atmospheric.HTU21DF/Samples/HTU21DFSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using Netduino.Foundation.Sensors.Atmospheric;
+
+namespace HTU21D_FTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            HTU21DF htu21d = new HTU21DF();
+        }
+    }
+}
