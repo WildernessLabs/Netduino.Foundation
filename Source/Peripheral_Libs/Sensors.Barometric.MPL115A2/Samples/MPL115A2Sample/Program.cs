04e39da8e41fcdb564cfeb9f5908255c9a40e504
Nevyn <mark@thepcsite.co.uk>
2017-12-18 10:43:06 +0000

    MPL115A2 - Added sample

---
+++ Source/Peripheral_Libs/Sensors.Barometric.MPL115A2/Samples/MPL115A2Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Barometric;
+
+namespace MPL115A2Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var mpl115a2 = new MPL115A2();
+
+            Debug.Print("MPL115A2 Test");
+            while (true)
+            {
+                mpl115a2.Read();
+                Debug.Print("Pressure: " + mpl115a2.Pressure.ToString("f2") + " kPa, Temperature: " +
+                            mpl115a2.Temperature.ToString("f2") + "C");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
