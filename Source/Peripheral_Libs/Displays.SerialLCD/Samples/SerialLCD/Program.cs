7b9895da43e3abf989e8855dce357de49fab1891
Nevyn <mark@thepcsite.co.uk>
2017-12-18 09:40:36 +0000

    SerialLCD - Added Sample.

---
+++ Source/Peripheral_Libs/Displays.SerialLCD/Samples/SerialLCD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Netduino.Foundation.Displays;
+
+namespace SerialLCDTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var display = new SerialLCD();
+            //
+            //  Clear the display ready for the test.
+            //
+            display.Clear();
+            display.SetCursorStyle(SerialLCD.CursorStyle.BlinkingBoxOff);
+            display.SetCursorStyle(SerialLCD.CursorStyle.UnderlineOff);
+            //
+            //  Display some text on the bottom row of a 16x2 LCD.
+            //
+            display.SetCursorPosition(1, 1);
+            display.DisplayText("Hello, world");
+            Thread.Sleep(1000);
+        }
+    }
+}
