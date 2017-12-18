5a249b246edd9dae6d57d9eea7d61c52927b7cae
Nevyn <mark@thepcsite.co.uk>
2017-12-18 11:10:28 +0000

    ADXL345 - Added sample

---
+++ Source/Peripheral_Libs/Sensors.Motion.ADXL345/Samples/ADXL345Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Motion;
+
+namespace ADXL345Sample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("\n\n");
+            Debug.Print("ADXL345 Register Test Application.");
+            Debug.Print("----------------------------------");
+            var adxl345 = new ADXL345();
+            Debug.Print("Device ID: " + adxl345.DeviceID);
+            adxl345.SetPowerState(false, false, true, false, ADXL345.Frequency.EightHz);
+            while (true)
+            {
+                adxl345.Read();
+                Debug.Print("X: " + adxl345.X + ", Y: " + adxl345.Y + ", Z: " + adxl345.Z);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
