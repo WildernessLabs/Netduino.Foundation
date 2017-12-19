eab1861c6cbe2336b9c2c970bca72ecfe53e5f35
Nevyn <mark@thepcsite.co.uk>
2017-12-19 07:59:38 +0000

    ParallaxPIR - Added sample.

---
+++ Source/Peripheral_Libs/Sensors.Motion.ParallaxPIR/Samples/ParallaxPIRSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Motion;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using System.Threading;
+
+namespace ParallaxPIRSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ParallaxPIR pir = new ParallaxPIR(Pins.GPIO_PIN_D8);
+
+            pir.OnMotionStart += pir_OnMotionStart;
+            pir.OnMotionEnd += pir_OnMotionEnd;
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void pir_OnMotionEnd(object sender)
+        {
+            Debug.Print("Motion stopped.");
+        }
+
+        static void pir_OnMotionStart(object sender)
+        {
+            Debug.Print("Motion detected.");
+        }
+    }
+}
