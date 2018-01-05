7a9a7f815a71f9091f2ff28c40805d50c6cad2de
bryan costanich <b@wowzer.net>
2018-01-04 22:29:22 -0800

    added SpstSwitch.

---
+++ Source/Netduino.Foundation.Core.Samples/SpstSwitch/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using H = Microsoft.SPOT.Hardware;
+using N = SecretLabs.NETMF.Hardware.Netduino;
+using Netduino.Foundation.Sensors.Switches;
+
+namespace SpstSwitch_Sample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var spstSwitch = new SpstSwitch(N.Pins.GPIO_PIN_D0, SwitchCircuitTerminationType.High);
+
+            Debug.Print("Initial switch state, isOn: " + spstSwitch.IsOn.ToString());
+
+            spstSwitch.Changed += (s, e) =>
+            {
+                Debug.Print("Switch Changed");
+                Debug.Print("Switch on: " + spstSwitch.IsOn.ToString());
+            };
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+         }
+    }
+}
