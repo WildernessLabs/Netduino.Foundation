0e6d3e5cad1689fca23c00cf7ac4545a80fdde3a
NevynUK <mark@thepcsite.co.uk>
2018-01-07 10:16:57 +0000

    SI7021 - Added missing property assignments.

--- Source/Peripheral_Libs/Sensors.Atmospheric.SI7021/Driver/SI7021.cs
+++ Source/Peripheral_Libs/Sensors.Atmospheric.SI7021/Driver/SI7021.cs
@@ -237,6 +237,9 @@ namespace Netduino.Foundation.Sensors.Atmospheric
                 throw new ArgumentOutOfRangeException(nameof(updateInterval), "Update period should be 0 or >= than " + MINIMUM_POLLING_PERIOD);
             }
 
+            TemperatureChangeNotificationThreshold = temperatureChangeNotificationThreshold;
+            HumidityChangeNotificationThreshold = humidityChangeNotificationThreshold;
+            _updateInterval = updateInterval;
             _si7021 = new I2CBus(address, speed);
             //
             //  Get the device ID.
