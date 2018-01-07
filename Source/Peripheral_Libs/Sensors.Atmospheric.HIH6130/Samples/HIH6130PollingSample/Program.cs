34a32f9443ff080c5c9cce455c9123cf6ce89997
NevynUK <mark@thepcsite.co.uk>
2018-01-07 16:21:12 +0000

    HIH6130 - Added polling sample.

---
+++ Source/Peripheral_Libs/Sensors.Atmospheric.HIH6130/Samples/HIH6130PollingSample/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.SPOT;
+
+namespace HIH6130PollingSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print(Resources.GetString(Resources.StringResources.String1));
+        }
+    }
+}
