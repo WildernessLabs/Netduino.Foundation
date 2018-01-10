6d831d8af4f1b4527fdeeae1ec13cb94c60ef2da
NevynUK <mark@thepcsite.co.uk>
2018-01-10 15:52:01 +0000

    MPL115A2 - Polling and Interrupt samples created.

---
+++ Source/Peripheral_Libs/Sensors.Barometric.MPL115A2/Samples/MPL115A2InterruptSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Barometric;
+
+namespace MPL115A2InterruptSample
+{
+    /// <summary>
+    ///     This application illustrates how to use the interrupt (event)
+    ///     handlers in the MPL115A2 sensor to obtain temperature and pressure
+    ///     readings.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            //
+            //  Create a new MPL object and set the temperature change threashold to 0.1C
+            //  and leave the pressure threshold set to the default 10 kPa.  Have the
+            //  sensor check the current readings every 0.5 seconds (500 milliseconds)
+            //
+            var mpl115a2 = new MPL115A2(updateInterval: 500, temperatureChangeNotificationThreshold: 0.1F);
+
+            Debug.Print("MPL115A2 Interrupt Test");
+            //
+            //  Attach interrupt handlers to the temperature and pressure sensor.
+            //
+            mpl115a2.PressureChanged += (s, e) =>
+            {
+                Debug.Print("Pressure: " + e.CurrentValue.ToString("f2"));
+            };
+
+            mpl115a2.TemperatureChanged += (s, e) =>
+            {
+                Debug.Print("Temperature: " + e.CurrentValue.ToString("f2") + "C");
+            };
+            //
+            //  Application can go to sleep now as readings will be dealt with by the 
+            //  interrupt handlers.
+            //
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ Source/Peripheral_Libs/Sensors.Barometric.MPL115A2/Samples/MPL115A2PollingSample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Barometric;
+
+namespace MPL115A2PollingSample
+{
+    /// <summary>
+    ///     This application illustrates how to use the MPL115A2 temperature
+    ///     in polling mode.  I'e' it is the responsibility of the application
+    ///     to check the sensor readings as required.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            //
+            //  Create a new MPL115A2 sensor object and set to polling mode
+            //  i.e. update period is 0 milliseconds.
+            //
+            var mpl115a2 = new MPL115A2(updateInterval: 0);
+
+            Debug.Print("MPL115A2 Polling Test");
+            while (true)
+            {
+                //
+                //  Have the sensor make new readings.
+                //
+                mpl115a2.Update();
+                //
+                //  Display the values in the debug console.
+                //
+                Debug.Print("Pressure: " + mpl115a2.Pressure.ToString("f2") + " kPa, Temperature: " +
+                            mpl115a2.Temperature.ToString("f2") + "C");
+                //
+                //  Sleep for a while (1 second) before taking the next readins.
+                //
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
