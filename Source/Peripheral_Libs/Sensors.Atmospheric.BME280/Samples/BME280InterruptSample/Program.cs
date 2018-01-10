560311511936d0873c35a7c7c19ad6523c6bf31a
NevynUK <mark@thepcsite.co.uk>
2018-01-10 15:49:57 +0000

    BME280 - Polling sample

--- Source/Peripheral_Libs/Sensors.Atmospheric.BME280/Samples/BME280InterruptSample/Program.cs
+++ Source/Peripheral_Libs/Sensors.Atmospheric.BME280/Samples/BME280InterruptSample/Program.cs
@@ -6,30 +6,45 @@ using Netduino.Foundation.Sensors;
 
 namespace BME280InterruptSample
 {
+    /// <summary>
+    ///     This sample illustrates how to use the interrupt (events) to obtain
+    ///     temperature, humidity and pressures readsing when the values change
+    ///     outside of the specified thresholds.
+    /// </summary>
     public class Program
     {
-        /// <summary>
-        ///     BME280 temperature, humidity and pressure object.
-        /// </summary>
-        private static readonly BME280 _sensor = new BME280();
-
-        /// <summary>
-        ///     Main program loop.
-        /// </summary>
         public static void Main()
         {
-            _sensor.HumidityChanged += (s, e) =>
+            //
+            //  BME280 temperature, humidity and pressure object.  This object should 
+            //  raise an interrupt when the changes in the sensor readings exceed the 
+            //  following:
+            //
+            //  Temperature: +/- 1 C
+            //  Humidity: +/- 1 %
+            //  Pressure: +/- 10 kPa (the default value for this threshold)
+            //
+            BME280 sensor = new BME280(temperatureChangeNotificationThreshold: 0.1F,
+                humidityChangeNotificationThreshold: 1.0f);
+            //
+            //  Attach interrupt handlers to the temperature and pressure sensor.
+            //
+            sensor.HumidityChanged += (s, e) =>
             {
-                Debug.Print("Current humidity: " + e.CurrentValue);
+                Debug.Print("Current humidity: " + e.CurrentValue.ToString("f2"));
             };
-            _sensor.PressureChanged += (s, e) =>
+            sensor.PressureChanged += (s, e) =>
             {
-                Debug.Print("Current pressure: " + e.CurrentValue);
+                Debug.Print("Current pressure: " + (e.CurrentValue / 100).ToString("f2"));
             };
-            _sensor.TemperatureChanged += (s, e) =>
+            sensor.TemperatureChanged += (s, e) =>
             {
-                Debug.Print("Current temperature: " + e.CurrentValue);
+                Debug.Print("Current temperature: " + e.CurrentValue.ToString("f2"));
             };
+            //
+            //  Application can go to sleep now as readings will be dealt with by the 
+            //  interrupt handlers.
+            //
             Thread.Sleep(Timeout.Infinite);
         }
     }
--- Source/Peripheral_Libs/Sensors.Atmospheric.BME280/Samples/BME280PollingSample/Program.cs
+++ Source/Peripheral_Libs/Sensors.Atmospheric.BME280/Samples/BME280PollingSample/Program.cs
@@ -4,20 +4,36 @@ using System.Threading;
 
 namespace BME280_Sample
 {
+    /// <summary>
+    ///     Illustrate how to use the BME280 in polling mode.
+    /// </summary>
     public class Program
     {
         public static void Main()
         {
+            //
+            //  Create a new BME280 object and put the sensor into polling
+            //  mode (update intervale set to 0ms).
+            //
             BME280 sensor = new BME280(updateInterval: 0);
 
             string message;
             while (true)
             {
+                //
+                //  Make the sensor take new readings.
+                //
                 sensor.Update();
+                //
+                //  Prepare a message for the user and output to the debug console.
+                //
                 message = "Temperature: " + sensor.Temperature.ToString("F1") + " C\n";
                 message += "Humidity: " + sensor.Humidity.ToString("F1") + " %\n";
                 message += "Pressure: " + (sensor.Pressure / 100).ToString("F0") + " hPa\n\n";
                 Debug.Print(message);
+                //
+                //  Sleep for 1000ms before repeating the process.
+                //
                 Thread.Sleep(1000);
             }
         }
