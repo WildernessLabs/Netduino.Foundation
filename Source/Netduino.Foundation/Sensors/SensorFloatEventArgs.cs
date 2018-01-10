e177c2b373194f0224ba0076ff6c635006c5e30e
NevynUK <mark@thepcsite.co.uk>
2018-01-10 07:02:31 +0000

    SensorFloatEventArgs - Added comments.

--- Source/Netduino.Foundation/Sensors/SensorFloatEventArgs.cs
+++ Source/Netduino.Foundation/Sensors/SensorFloatEventArgs.cs
@@ -1,13 +1,28 @@
-using System;
 using Microsoft.SPOT;
 
 namespace Netduino.Foundation.Sensors
 {
+    /// <summary>
+    ///     Class to be used when sending floating point sensor data to the application
+    ///     through the eventing mechanism.
+    /// </summary>
     public class SensorFloatEventArgs : EventArgs
     {
+        /// <summary>
+        ///     Last value read from the sensor AND sent to the user in a notification.
+        /// </summary>
         public float LastNotifiedValue { get; set; }
+
+        /// <summary>
+        ///     Current sensor reading.
+        /// </summary>
         public float CurrentValue { get; set; }
 
+        /// <summary>
+        ///     Constructor for SensorFloatEventArgs objects.
+        /// </summary>
+        /// <param name="lastValue">Last sensor value sent through the eventing mechanism.</param>
+        /// <param name="currentValue">Current sensor reading.</param>
         public SensorFloatEventArgs (float lastValue, float currentValue)
         {
             LastNotifiedValue = lastValue;
@@ -15,5 +30,10 @@ namespace Netduino.Foundation.Sensors
         }
     }
 
+    /// <summary>
+    ///     Delegate for the events that will return a SensorFloatEventArgs object.
+    /// </summary>
+    /// <param name="sender">Object sending the notification.</param>
+    /// <param name="e">SensorFloatEventArgs object containing the data for the application.</param>
     public delegate void SensorFloatEventHandler(object sender, SensorFloatEventArgs e);
 }
