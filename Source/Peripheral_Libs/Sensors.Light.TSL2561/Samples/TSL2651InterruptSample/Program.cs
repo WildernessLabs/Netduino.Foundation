c22ea936e8e4294d67100a09bd84e5a06064bfed
NevynUK <mark@thepcsite.co.uk>
2018-01-10 15:53:11 +0000

    TSL2651 - Created polling and interrupt samples.

---
+++ Source/Peripheral_Libs/Sensors.Light.TSL2561/Samples/TSL2651InterruptSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Light;
+
+namespace TSL2561InterruptExample
+{
+    /// <summary>
+    ///     This application illustrates how to use the TSL2561 light sensor
+    ///     operating in interrupt mode for changes in light level.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            //
+            //  Create a TSL2561 object operating in interrupt mode.
+            //
+            TSL2561 tsl2561 = new TSL2561();
+            //
+            //  Set up the sensor.
+            //
+            Debug.Print("TSL2561 Interrupt Application.");
+            Debug.Print("Device ID: " + tsl2561.ID);
+            tsl2561.TurnOff();
+            tsl2561.SensorGain = TSL2561.Gain.High;
+            tsl2561.Timing = TSL2561.IntegrationTiming.Ms402;
+            tsl2561.TurnOn();
+            tsl2561.LightLevelChanged += (s, e) =>
+            {
+                Debug.Print("Light intensity: " + e.CurrentValue.ToString("f2"));
+            };
+            //
+            //  Put the application to sleep as the interrupt handler will deal
+            //  with any changes in sensor data.
+            //
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
--- Source/Peripheral_Libs/Sensors.Light.TSL2561/Samples/TSL2651PollingSample/Program.cs
+++ Source/Peripheral_Libs/Sensors.Light.TSL2561/Samples/TSL2651PollingSample/Program.cs
@@ -2,14 +2,17 @@
 using Microsoft.SPOT;
 using Netduino.Foundation.Sensors.Light;
 
-namespace TSL2561Example
+namespace TSL2651PollingSample
 {
+    /// <summary>
+    ///     This application illustrates how to use the TSLK2561
+    ///     in polling mode.
+    /// </summary>
     public class Program
     {
-        private static readonly TSL2561 tsl2561 = new TSL2561();
-
         public static void Main()
         {
+            TSL2561 tsl2561 = new TSL2561();
             Debug.Print("Polled TSL2561 Application.");
             Debug.Print("Device ID: " + tsl2561.ID);
             tsl2561.TurnOff();
@@ -17,17 +20,13 @@ namespace TSL2561Example
             tsl2561.Timing = TSL2561.IntegrationTiming.Ms402;
             tsl2561.TurnOn();
             //
-            //  Wait for at least one integration cycle (set to 402 milliseconds above).
-            //
-            Thread.Sleep(500);
-            //
             //  Repeatedly read the light intensity.
             //
             while (true)
             {
                 var adcData = tsl2561.SensorReading;
                 Debug.Print("Data0: " + adcData[0] + ", Data1: " + adcData[1]);
-                Debug.Print("Light intensity: " + tsl2561.Lux);
+                Debug.Print("Light intensity: " + tsl2561.LightLevel);
                 Thread.Sleep(2000);
             }
         }
