c30b9076ca466ebb7c64f5dd70e4b799a1a60354
NevynUK <mark@thepcsite.co.uk>
2018-01-11 15:57:41 +0000

    AnalogTemperature - Added interrupt and polling samples.

---
+++ Source/Peripheral_Libs/Sensors.Temperature.Analog/Samples/AnalogTemperatureInterruptSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Temperature.Analog;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace AnalogTemperatureInterruptSample
+{
+    /// <summary>
+    ///     Illustrate how to read the TMP35 temperature sensor in interrupt mode.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Read TMP35");
+            //
+            //  Create a new TMP35 object to check the temperature every 1s and
+            //  to report any changes over 0.1C.
+            //
+            var tmp35 = new AnalogTemperatureSensor(AnalogChannels.ANALOG_PIN_A0,
+                AnalogTemperatureSensor.SensorType.TMP35, updateInterval: 1000, 
+                temperatureChangeNotificationThreshold: 0.1F);
+            //
+            //  Connect an interrupt handler.
+            //
+            tmp35.TemperatureChanged += (s, e) =>
+            {
+                Debug.Print("Temperature: " + e.CurrentValue.ToString("f2"));
+            };
+            //
+            //  Now put the application to sleep as the data is processed
+            //  by the interrupt handler above.
+            //
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ Source/Peripheral_Libs/Sensors.Temperature.Analog/Samples/AnalogTemperaturePollingSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Temperature.Analog;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace AnalogTemperaturePollingSample
+{
+    /// <summary>
+    ///     Illustrate how to read the TMP35 temperature sensor (polling mode).
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Read TMP35");
+            var tmp35 = new AnalogTemperatureSensor(AnalogChannels.ANALOG_PIN_A0,
+                                                    AnalogTemperatureSensor.SensorType.TMP35, 
+                                                    updateInterval: 0);
+            //
+            //  Now read the sensor every 5 seconds.
+            //
+            while (true)
+            {
+                tmp35.Update();
+                Debug.Print("Reading: " + tmp35.Temperature.ToString("f2"));
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
