2d3840c9dff8981fec10aa20a24f82bd072861e9
NevynUK <mark@thepcsite.co.uk>
2018-01-11 15:57:58 +0000

    TMP102 - Added interrupt and polling samples.

---
+++ Source/Peripheral_Libs/Sensors.Temperature.TMP102/Samples/TMP102InterruptSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Temperature;
+
+namespace TMP102InterruptSample
+{
+    /// <summary>
+    ///     Illustrate how to read the TMP102 using interrupts.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("TMP102 Interrupt Sample");
+            //
+            //  Create a new TMP102 object, check the termperature every
+            //  1s and report any changes grater than +/- 0.1C.
+            //
+            var tmp102 = new TMP102(updateInterval: 1000, temperatureChangeNotificationThreshold: 0.1F);
+            //
+            //  Hook up the interrupt handler.
+            //
+            tmp102.TemperatureChanged += (s, e) =>
+            {
+                Debug.Print("Temperature: " + e.CurrentValue.ToString("f2"));
+            };
+            //
+            //  Now put the main program to sleep as the interrupt handler
+            //  above deals with processing the sensor data.
+            //
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ Source/Peripheral_Libs/Sensors.Temperature.TMP102/Samples/TMP102PollingSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Netduino.Foundation.Sensors.Temperature;
+
+namespace TMP102PollingSample
+{
+    /// <summary>
+    ///     Illustrate how to read the temperature from a TMP102
+    ///     by polling the sensor.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("TMP102 Polling Sample");
+            var tmp102 = new TMP102(updateInterval: 0);
+            //
+            //  Read the TMP102 every second and display the temperature.
+            //
+            while (true)
+            {
+                tmp102.Update();
+                Debug.Print("Temperature: " + tmp102.Temperature.ToString("f2"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
