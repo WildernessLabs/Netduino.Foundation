3b197bf9c76db79364a60a1abc10debf3af89083
bryan costanich <b@wowzer.net>
2018-01-12 19:01:25 -0800

    added a second motor.

--- Source/Netduino.Foundation.Core.Samples/HBridgeMotor/Program.cs
+++ Source/Netduino.Foundation.Core.Samples/HBridgeMotor/Program.cs
@@ -10,16 +10,20 @@ namespace HBridgeMotor_Sample
         public static void Main()
         {
             var motor1 = new HBridgeMotor(N.PWMChannels.PWM_PIN_D3, N.PWMChannels.PWM_PIN_D5, N.Pins.GPIO_PIN_D4);
+            var motor2 = new HBridgeMotor(N.PWMChannels.PWM_PIN_D6, N.PWMChannels.PWM_PIN_D10, N.Pins.GPIO_PIN_D7);
 
             while (true)
             {
-                // set the speed to 100% forward
+                // set the speed on both motors to 100% forward
                 motor1.Speed = 1f;
+                motor2.Speed = 1f;
                 Thread.Sleep(1000);
                 motor1.Speed = 0f;
+                motor2.Speed = 0f;
                 Thread.Sleep(500);
                 // 100% reverse
                 motor1.Speed = -1f;
+                motor2.Speed = -1f;
                 Thread.Sleep(1000);
             }
         }
