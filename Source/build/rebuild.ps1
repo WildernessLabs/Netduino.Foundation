9cb50c79bb03230a913295410e3c4d5aa6cf4b1e
bryan costanich <b@wowzer.net>
2018-01-15 23:07:46 -0800

    commented out unfinished packages.

--- Source/build/rebuild.ps1
+++ Source/build/rebuild.ps1
@@ -16,7 +16,7 @@ nuget pack Peripheral_Libs/ICs.EEPROM.AT24Cxx/Driver/ICs.EEPROM.AT24Cxx.csproj -
 nuget pack Peripheral_Libs/RTCs.DS323x/Driver/RTCs.DS323x.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Shields.AdafruitMotorShield/Driver/Shields.AdafruitMotorShield.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Atmospheric.BME280/Driver/Sensors.Atmospheric.BME280.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Atmospheric.BMP085/Driver/Sensors.Atmospheric.BMP085.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Atmospheric.BMP085/Driver/Sensors.Atmospheric.BMP085.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Atmospheric.GroveTH02/Driver/Sensors.Atmospheric.GroveTH02.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Atmospheric.HIH6130/Driver/Sensors.Atmospheric.HIH6130.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Atmospheric.HTU21DF/Driver/Sensors.Atmospheric.HTU21DF.csproj -NoDefaultExcludes -Prop Configuration=Release
@@ -24,27 +24,27 @@ nuget pack Peripheral_Libs/Sensors.Barometric.MPL115A2/Driver/Sensors.Barometric
 nuget pack Peripheral_Libs/Sensors.Barometric.MPL3115A2/Driver/Sensors.Barometric.MPL3115A2.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Atmospheric.SHT31D/Driver/Sensors.Atmospheric.SHT31D.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Atmospheric.SI7021/Driver/Sensors.Atmospheric.SI7021.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Compass.Grove3AxisDigitalCompass/Driver/Sensors.Compass.Grove3AxisDigitalCompass.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Distance.SharpGP2D12/Driver/Sensors.Distance.SharpGP2D12.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Compass.Grove3AxisDigitalCompass/Driver/Sensors.Compass.Grove3AxisDigitalCompass.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Distance.SharpGP2D12/Driver/Sensors.Distance.SharpGP2D12.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.GPS.NMEA/Driver/Sensors.GPS.NMEA.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Light.ALSPT19315C/Driver/Sensors.Light.ALSPT19315C.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Light.SI1145/Driver/Sensors.Light.SI1145.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Light.ALSPT19315C/Driver/Sensors.Light.ALSPT19315C.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Light.SI1145/Driver/Sensors.Light.SI1145.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Light.TSL2561/Driver/Sensors.Light.TSL2561.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Motion.ADXL335/Driver/Sensors.Motion.ADXL335.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Motion.ADXL345/Driver/Sensors.Motion.ADXL345.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Motion.ADXL362/Driver/Sensors.Motion.ADXL362.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Motion.BNO055/Driver/Sensors.Motion.BNO055.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Motion.FXAS21002/Driver/Sensors.Motion.FXAS21002.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Motion.FXOS8700CQ/Driver/Sensors.Motion.FXOS8700CQ.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Motion.FXAS21002/Driver/Sensors.Motion.FXAS21002.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Motion.FXOS8700CQ/Driver/Sensors.Motion.FXOS8700CQ.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Motion.MAG3110/Driver/Sensors.Motion.MAG3110.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Motion.Memsic2125/Driver/Sensors.Motion.Memsic2125.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Motion.MPU6050/Driver/Sensors.Motion.MPU6050.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Motion.Memsic2125/Driver/Sensors.Motion.Memsic2125.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Motion.MPU6050/Driver/Sensors.Motion.MPU6050.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Temperature.Analog/Driver/Sensors.Temperature.Analog.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Sensors.Temperature.DS18B20/Driver/Sensors.Temperature.DS18B20.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Sensors.Temperature.DS18B20/Driver/Sensors.Temperature.DS18B20.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Sensors.Temperature.TMP102/Driver/Sensors.Temperature.TMP102.csproj -NoDefaultExcludes -Prop Configuration=Release
 nuget pack Peripheral_Libs/Servos.Servo/Driver/Servos.Servo.Core.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Shields.AdafruitMotorShield/Driver/Shields.AdafruitMotorShield.csproj -NoDefaultExcludes -Prop Configuration=Release
-nuget pack Peripheral_Libs/Shields.SparkfunWeatherShield/Driver/Shields.SparkfunWeatherShield.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Shields.AdafruitMotorShield/Driver/Shields.AdafruitMotorShield.csproj -NoDefaultExcludes -Prop Configuration=Release
+# nuget pack Peripheral_Libs/Shields.SparkfunWeatherShield/Driver/Shields.SparkfunWeatherShield.csproj -NoDefaultExcludes -Prop Configuration=Release
 #
 #   Command below moves the packages.
 #
