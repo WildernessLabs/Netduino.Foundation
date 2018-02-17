8117de2b5e336bce4655762f170ab4b27c7aaf96
bryan costanich <b@wowzer.net>
2018-02-17 12:51:47 -0800

    small cleanup.

--- Source/Peripheral_Libs/Displays.SerialLCD/Samples/SerialLCD4x20_Sample/Program.cs
+++ Source/Peripheral_Libs/Displays.SerialLCD/Samples/SerialLCD4x20_Sample/Program.cs
@@ -9,6 +9,7 @@ namespace SerialLCD4x20_Sample
 {
     public class Program
     {
+        // counts from 1-100 and displays it in a scrolling manner on a display
         public static void Main()
         {
             SerialLCD _lcd = new SerialLCD(new TextDisplayConfig { Height = 4, Width = 20 });
@@ -18,12 +19,12 @@ namespace SerialLCD4x20_Sample
             while (true) {
 
                 // count to 100 
-                for (int i = 1; i <= 100; i++) {
+                for (int i = 1; i <= (100 - _lcd.DisplayConfig.Height + 1); i++) {
                     // write to all lines
                     for (byte l = 0; l < _lcd.DisplayConfig.Height; l++) {
                         _lcd.WriteLine((i + l).ToString(), l);
                     }
-                    Thread.Sleep(500);
+                    Thread.Sleep(250);
                 }
 
             }
