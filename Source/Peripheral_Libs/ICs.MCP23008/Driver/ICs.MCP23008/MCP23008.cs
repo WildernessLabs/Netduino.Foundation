80331aadf8aa7b40b052c2719d763f7aa0ebaa8a
bryan costanich <b@wowzer.net>
2018-03-10 19:47:46 -0800

    early draft of port stuff.

--- Source/Peripheral_Libs/ICs.MCP23008/Driver/ICs.MCP23008/MCP23008.cs
+++ Source/Peripheral_Libs/ICs.MCP23008/Driver/ICs.MCP23008/MCP23008.cs
@@ -8,6 +8,22 @@ namespace Netduino.Foundation.ICs
     {
         private readonly I2CBus _i2cBus;
 
+
+        // register addresses
+        // IO Direction - controls the direction of the GPIO
+        private const byte _IODirectionRegister = 0x00; //IODIR
+        private const byte _InputPolarityRegister = 0x01; //IPOL
+        private const byte _InterruptOnChangeRegister = 0x02; //GPINTEN
+        private const byte _DefaultComparisonValueRegister = 0x03; //DEFVAL
+        private const byte _InterruptControlRegister = 0x04; //INTCON
+        private const byte _IOConfigurationRegister = 0x05; //IOCON
+        private const byte _PullupResistorConfigurationRegister = 0x06; //GPPU
+        private const byte _InterruptFlagRegister = 0x07; //INTF
+        private const byte _InterruptCaptureRegister = 0x08; //INTCAP
+        private const byte _GPIORegister = 0x09; //GPIO
+        private const byte _OutputLatchRegister = 0x0A; //OLAT
+
+
         protected MCP23008()
         { }
 
