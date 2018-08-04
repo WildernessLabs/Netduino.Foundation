bde36d81f123a3abf13ffe890ceb9251b84b6d77
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-08-03 22:50:09 -0700

    [PiezoSpeaker] Added ToneFrequency with a large range of predefined tones and update PiezoSample project

--- Source/PiezoSample/Program.cs
+++ Source/PiezoSample/Program.cs
@@ -1,6 +1,6 @@
-using System;
-using Microsoft.SPOT;
 using Netduino.Foundation.Piezos;
+using System.Threading;
+using N = SecretLabs.NETMF.Hardware.Netduino;
 
 namespace PiezoSample
 {
@@ -8,7 +8,35 @@ namespace PiezoSample
     {
         public static void Main()
         {
-            PiezoSpeaker piezo;
+            const int NUMBER_TONES = 12;
+            float[] melody = new float[NUMBER_TONES] 
+            {
+                ToneFrequency.NOTE_C5,
+                ToneFrequency.NOTE_B4,
+                ToneFrequency.NOTE_G4,
+                ToneFrequency.NOTE_C5,
+                ToneFrequency.NOTE_B4,
+                ToneFrequency.NOTE_E4,
+                ToneFrequency.REST,
+                ToneFrequency.NOTE_C5,
+                ToneFrequency.NOTE_C4,
+                ToneFrequency.NOTE_G4,
+                ToneFrequency.NOTE_A4,
+                ToneFrequency.NOTE_C5
+            };
+            int[] beats = new int[NUMBER_TONES] { 16, 16, 16, 8, 8, 16, 32, 16, 16, 16, 8, 8 };
+
+            PiezoSpeaker piezo = new PiezoSpeaker(N.PWMChannels.PWM_PIN_D5);
+
+            while (true)
+            {
+                for(int i=0; i<NUMBER_TONES; i++)
+                {
+                    piezo.PlayTone(melody[i], beats[i]);
+                }
+                
+                Thread.Sleep(1000);
+            }
         }
     }
 }
