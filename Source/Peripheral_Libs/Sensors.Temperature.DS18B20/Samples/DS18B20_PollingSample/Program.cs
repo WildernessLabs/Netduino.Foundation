5a81b5e9b9f9316a36c87674ebda213ad3a8faca
NevynUK <mark@thepcsite.co.uk>
2018-08-14 19:19:05 +0100

    DS18B20: Polling sample tested.

--- Source/Peripheral_Libs/Sensors.Temperature.DS18B20/Samples/DS18B20_PollingSample/Program.cs
+++ Source/Peripheral_Libs/Sensors.Temperature.DS18B20/Samples/DS18B20_PollingSample/Program.cs
@@ -7,17 +7,17 @@ namespace DS18B20_PollingSample
 {
     public class Program
     {
-        //
-        //  This application demonstrates how to create a new DS18B20object and read
-        //  the temperature from the sensor.  The current temperature will be displayed
-        //  in the Debug Output window every 500-1250ms.
-        //
-        //  Note that the rate of display is dependent upon the time it take to perform
-        //  the conversion of the temperature into a digital value.
-        //
-        //  Connect the DS18B20 data pin to GPIO D3 and then apply power and ground to
-        //  the sensor.
-        //
+        /// <summary>
+        ///     This application demonstrates how to create a new DS18B20object and read
+        ///     the temperature from the sensor.  The current temperature will be displayed
+        ///     in the Debug Output window every 500-1250ms.
+        ///
+        ///     Note that the rate of display is dependent upon the time it take to perform
+        ///     the conversion of the temperature into a digital value.
+        ///
+        ///     Connect the DS18B20 data pin to GPIO D3 and then apply power and ground to
+        ///     the sensor.
+        /// </summary>
         public static void Main()
         {
             DS18B20 ds18b20 = new DS18B20(Pins.GPIO_PIN_D3, updateInterval: 0);
