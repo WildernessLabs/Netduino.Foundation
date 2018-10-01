03cc7e228f6e1c0eb39c7bfac4bb94c9898d6709
Adrian Stevens <adrian.stevens@gmail.com>
2018-09-30 18:30:35 -0700

    Added color format enum to DisplayBase and updated DisplayBase to implement IDisplay

--- Source/Netduino.Foundation/BaseClasses/DisplayBase.cs
+++ Source/Netduino.Foundation/BaseClasses/DisplayBase.cs
@@ -3,7 +3,7 @@
     /// <summary>
     ///     Define the interface for the display object.
     /// </summary>
-    public abstract class DisplayBase
+    public abstract class DisplayBase : IDisplay
     {
         /// <summary>
         /// 
@@ -15,6 +15,22 @@
             XOr,
             Copy
         };
+
+        public enum DisplayColorMode
+        {
+            Format1bpp, //1306 and single color ePaper
+            Format2bpp, //for 2 color ePaper
+            Format12bppRgb444, //TFT in 12 bit mode
+            Format16bppRgb555, //not used
+            Format16bppRgb565, //TFT in 16 bit mode
+            Format18bppRgb666, //TFT in 18 bit mode
+            Format24bppRgb888 //not used
+        }
+
+        public abstract DisplayColorMode ColorMode { get; }
+
+        public abstract uint Width { get; }
+        public abstract uint Height { get; }
 
         /// <summary>
         ///     Indicate of the hardware driver should ignore out of bounds pixels
@@ -41,18 +57,5 @@
         /// <param name="y"></param>
         /// <param name="colored"></param>
         public abstract void DrawPixel(int x, int y, bool colored);
-
-        /// <summary>
-        ///     Copy a bitmap to the display.
-        /// </summary>
-        /// <param name="x">Abscissa of the top left corner of the bitmap.</param>
-        /// <param name="y">Ordinate of the top left corner of the bitmap.</param>
-        /// <param name="width">Width of the bitmap.</param>
-        /// <param name="height">Height of the bitmap.</param>
-        /// <param name="bitmap">Bitmap to transfer</param>
-        /// <param name="bitmapMode">How should the bitmap be transferred to the display?</param>
-        public abstract void DrawBitmap(int x, int y, int width, int height, byte[] bitmap, BitmapMode bitmapMode);
-
-        public abstract void DrawBitmap(int x, int y, int width, int height, byte[] bitmap, Color color);
     }
 }
